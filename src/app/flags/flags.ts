9eada6dc422855eadc6bce380fa2ff8ddbdb9eb5
zeyden <zeydenlaxharoth@gmail.com>
2021-08-15 13:41:55 -0500

    default flags.

---
+++ src/app/flags/flags.ts
@@ -0,0 +1,8 @@
+export const flags:{[key: string]:any} = {
+  currentmap : 'map1',
+  currentroom: 'room1',
+  map1room1firstenter : true,
+  map1room1firstexit  : true,
+  firstreturn2room1   : true,
+  caninroom1          : true,
+}
