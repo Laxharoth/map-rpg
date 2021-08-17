284baaa0611976949024eed7bce27eb070adaa20
zeyden <zeydenlaxharoth@gmail.com>
2021-08-17 18:05:32 -0500

    Changed Subject to only send the effective status of the locks.

--- src/app/service/lock-map.service.ts
+++ src/app/service/lock-map.service.ts
@@ -6,30 +6,23 @@ import { Subject } from 'rxjs';
 })
 export class LockMapService {
 
-  private lockMapSubject = new Subject<{lockname:string,value:boolean}>();
+  private lockMapSubject = new Subject<boolean>();
 
   private mapLocks:{[key:string]:boolean}={};
-  constructor() {
-    this.onMapLockChanged().subscribe((data) => {
-      const lockname = data["lockname"];
-      const value    = data["value"];
-      this.mapLocks[lockname] = value;
-    })
-
-  }
+  constructor() {}
 
   lockMap(lockName)
   {
     //true means locked
-    this.lockMapSubject.next({lockname:lockName,value:true});
+    this.mapLocks[lockName] = true;
+    this.lockMapSubject.next(this.isMapLocked());
   }
-
   unlockMap(lockName:string)
   {
     //false means unlocked
-    this.lockMapSubject.next({lockname:lockName,value:false});
+    this.mapLocks[lockName] = false;
+    this.lockMapSubject.next(this.isMapLocked());
   }
-
   isMapLocked():boolean
   {
     return Object.keys(this.mapLocks).some(lockName => this.mapLocks[lockName]);
