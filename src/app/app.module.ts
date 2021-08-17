3f74b7cb71bde9dc9c5f5ad7a94589ae6578d207
zeyden <zeydenlaxharoth@gmail.com>
2021-08-17 18:23:58 -0500

    Added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MapComponent } from './component/map/map.component';
+import { GuiComponent } from './component/gui/gui.component';
+import { DescriptionComponent } from './component/description/description.component';
+import { DescriptionOptionsComponent } from './component/description-options/description-options.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent,
+    GuiComponent,
+    DescriptionComponent,
+    DescriptionOptionsComponent,
   ],
   imports: [
     BrowserModule,
