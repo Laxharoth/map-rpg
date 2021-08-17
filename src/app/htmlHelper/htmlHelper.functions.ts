b6212ce7a143ad7ae520e2f1e03227072b982da0
zeyden <zeydenlaxharoth@gmail.com>
2021-08-17 17:55:06 -0500

    Created file
    -Added function to get inputs from description.

---
+++ src/app/htmlHelper/htmlHelper.functions.ts
@@ -0,0 +1,8 @@
+export function getInputs():{input:string,select:string}
+{
+  const input:any = document.getElementById('unique-input');
+  const inputValue = input?.value||'';
+  const select:any = document.getElementById('unique-select')
+  const selectValue = select?.value||null;
+  return {input:inputValue, select:selectValue};
+}
