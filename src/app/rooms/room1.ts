805c2eae8bfac3def9bd7f641202c3f9fb6e1558
zeyden <zeydenlaxharoth@gmail.com>
2021-08-17 17:56:41 -0500

    returns template for the roomFunction instead of the actual function.

--- src/app/rooms/room1.ts
+++ src/app/rooms/room1.ts
@@ -1,25 +1,41 @@
 import { Description, DescriptionOptions } from "../classes/Description";
 import { Room } from "../classes/maps/room";
+import { getInputs } from "../htmlHelper/htmlHelper.functions";
 import { DescriptionHandlerService } from "../service/description-handler.service";
 import { FlagHandlerService } from "../service/flag-handler.service";
 import { MapHandlerService } from "../service/map-handler.service";
 
-export function room(roomName: string,flagshandler:FlagHandlerService,descriptionhandler:DescriptionHandlerService, maphandler:MapHandlerService):Room
+export function room(roomName: string)
 {
-  const $flag = (name:string,value:any=null) =>{
-    if(value!==null) flagshandler.setFlag(name,value);
-    return flagshandler.getFlag(name)
-  };
+  return function(flagshandler:FlagHandlerService,descriptionhandler:DescriptionHandlerService, maphandler:MapHandlerService):Room
+  {
+    const $flag = (name:string,value:any=null) =>{
+      if(value!==null) flagshandler.setFlag(name,value);
+      return flagshandler.getFlag(name)
+    };
 
-  const nextOption      = new DescriptionOptions("next",function(){descriptionhandler.nextDescription()});
+  const nextOption      = new DescriptionOptions("next",function(){descriptionhandler.nextDescription(false)});
   const yesOption      = (action:()=>void)=>new DescriptionOptions("Yes",function(){action()});
   const noOption      = new DescriptionOptions("No",function(){descriptionhandler.nextDescription()});
-  const furtherDescription = new Description(function(){return `There is nothing to do.`},[nextOption])
-
+  const nextOptionInputs = new DescriptionOptions("next",function(){
+    const {input,select} = getInputs();
+    $flag('petshout',input);
+    if(input===''){$flag('petshout',null);}
+    if(select){$flag('pet',select);}
+    descriptionhandler.nextDescription(false)
+  });
+  //with input and select
+  const furtherDescription = new Description(function(){return `There is \\input{"default":"${$flag('petshout')||''}","placeholder":"nothing"}\\ to do. Except to select \\select["cat","dog"]\\ but does nothing`+
+  //without input with select
+  //const furtherDescription = new Description(function(){return `There is to do. Except to select \\select["cat","dog"]\\ but does nothing`+
+  //with input without select
+  //const furtherDescription = new Description(function(){return `There is \\input{"placeholder":"nothing"}\\ to do. Except to select but does nothing\n\n`+
+  `${($flag('pet'))?`\n\nOMG there is a ${$flag('pet')}`:``}`+
+  `${($flag('pet')&&$flag('petshout'))?` 'it's saying ${$flag('petshout')}'`:``}`},[nextOptionInputs])
   const roomOptions =[
     new DescriptionOptions("option1",function(){
       descriptionhandler.headDescription(furtherDescription);
-      descriptionhandler.setDescription();
+      descriptionhandler.setDescription(false);
     }),
     new DescriptionOptions("save",function(){
       flagshandler.save("save1");
@@ -40,19 +56,19 @@ export function room(roomName: string,flagshandler:FlagHandlerService,descriptio
         }
         option.disabled = true;
           this.text = "unlock 3";
-      }
-    }),
-    new DescriptionOptions("option5",function(){}),
-    null,
-    new DescriptionOptions("option6",function(){}),
+        }
+      }),
+      new DescriptionOptions("add 1 hour",function(){ flagshandler.addTime("1h") }),
+      null,
+    new DescriptionOptions("add 1 month",function(){ flagshandler.addTime("1M") }),
   ]
   if($flag("caninroom1")){
-      roomOptions.splice(2,0,new DescriptionOptions("kick can",function(){
+    roomOptions.splice(2,0,new DescriptionOptions("kick can",function(){
         descriptionhandler.headDescription(kickCanDescription);
         descriptionhandler.setDescription();
         $flag("caninroom1",false);
         roomOptions.splice(2,1);
-    }))
+      }))
   }
   if(roomName==='room20'){
     const flyDescription1 =  new Description(function(){return `AAAAAAAAh`},[nextOption])
@@ -62,22 +78,22 @@ export function room(roomName: string,flagshandler:FlagHandlerService,descriptio
     ,[yesOption(()=>{
       descriptionhandler.nextDescription();
       descriptionhandler.tailDescription(flyDescription1, flyDescription2,flyDescription3);
+      flagshandler.addTime('30m');
       maphandler.loadRoom('room1');
     }),noOption])
     roomOptions.splice(2,0, new DescriptionOptions("Cannon",function(){
       descriptionhandler.headDescription(cannonDescription);
       descriptionhandler.setDescription();
       })
-    )
-  }
-  const fistEnter       = new Description(function(){return `It's the first time`},[nextOption]);
-  const roomDescription = new Description(function(){return `I look at the${(roomName!=='room1')?' same':''} room ${$flag("map1room1firstenter")?"FOR THE VERY FIRST TIME":"AGAIN."}
-    ${(roomName!=='room1')?`but it's room '${roomName}'`:''}
+      )
+    }
+    const fistEnter       = new Description(function(){return `It's the first time`},[nextOption]);
+  const roomDescription = new Description(function(){return `I look at the${(roomName!=='room1')?' same':''} room ${$flag("map1room1firstenter")?"FOR THE VERY FIRST TIME":"AGAIN."}${(roomName!=='room1')?`\nbut it's room '${roomName}'`:''}
   `}
   ,roomOptions)
   const firstExit = new Description(function(){return `It was the first time`},[nextOption]);
   const kickCanDescription =  new Description(function(){return `You kick the can, it's fun.
-                                  The can flew away`}
+  The can flew away`}
                               ,[nextOption]
                               )
 
@@ -98,10 +114,12 @@ export function room(roomName: string,flagshandler:FlagHandlerService,descriptio
       }
     },
     beforeMoveTo(roomName){
+      if(roomName!=='room1')
+      {flagshandler.addTime('5m')}
       return true;
     },
     icon:""
-    })
+  })
   return room;
 }
-
+}
