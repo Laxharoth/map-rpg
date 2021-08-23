36de4db668b0b64a3e5e4a443d38065b83d44bed
zeyden <zeydenlaxharoth@gmail.com>
2021-08-23 16:54:46 -0500

    Created subclass of Item

---
+++ src/app/classes/Equipment/Equipment.ts
@@ -0,0 +1,20 @@
+import { battleActionOutput, statusHelper } from "src/app/customTypes/customTypes";
+import { tag } from "src/app/customTypes/tags";
+import { Character } from "../Character/Character";
+import { Item } from "../Items/Item";
+
+export abstract class Equipment extends Item
+{
+  //Check if can equip
+  abstract get name():string;
+  abstract canEquip(character:Character ):boolean;
+  abstract applyModifiers(character:Character):void;
+  abstract get tags():tag[];
+
+  get isBattleUsable(): boolean{return true;};
+  get isPartyUsable(): boolean {return true;};
+  get isEnemyUsable(): boolean {return false;};
+  get isSelfUsableOnly(): boolean {return true;};
+
+  itemEffect(user:Character,target: Character): battleActionOutput { return [[],[]] }
+}
