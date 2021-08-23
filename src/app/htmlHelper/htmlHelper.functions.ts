f4556b3562df911dca47dc5c2fd53a4f1af7e192
zeyden <zeydenlaxharoth@gmail.com>
2021-08-23 16:41:01 -0500

    Added pushBattleActionOutput Added randomBetween Added ranfomCheck

--- src/app/htmlHelper/htmlHelper.functions.ts
+++
@@ -1,8 +0,0 @@
-export function getInputs():{input:string,select:string}
-{
-  const input:any = document.getElementById('unique-input');
-  const inputValue = input?.value||'';
-  const select:any = document.getElementById('unique-select')
-  const selectValue = select?.value||null;
-  return {input:inputValue, select:selectValue};
-}
