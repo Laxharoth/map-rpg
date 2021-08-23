0bb081df77c07c5f248845663ec7110977d0cc75
zeyden <zeydenlaxharoth@gmail.com>
2021-08-23 16:35:49 -0500

    Replaced services with masterService Added characterStats Added damagedTypes Added battleActionOutput

--- src/app/customTypes/customTypes.ts
+++ src/app/customTypes/customTypes.ts
@@ -1,8 +1,12 @@
+import { Status } from "../classes/Character/Status/Status";
+import { Description } from "../classes/Descriptions/Description";
 import { Room } from "../classes/maps/room";
-import { DescriptionHandlerService } from "../service/description-handler.service";
-import { FlagHandlerService } from "../service/flag-handler.service";
-import { MapHandlerService } from "../service/map-handler.service";
+import { MasterService } from "../classes/masterService";
 
-export type roomFunction = (flagshandler: FlagHandlerService, descriptionhandler: DescriptionHandlerService, maphandler: MapHandlerService) => Room;
-export type inputObject  = {default: string , placeholder: string}
-export type characterStats = { hitpoints     : number; energypoints  : number; attack  : number; defence : number; speed   : number; }
+export type roomFunction = (masterService:MasterService) => Room;
+export type inputObject = {default: string , placeholder: string}
+export type characterStats = { hitpoints : number; energypoints : number; 
+                               attack : number; aim: number; defence : number; speed : number; evasion : number;
+                               heatresistance: number; energyresistance:number; frostresistance:number; slashresistance: number; bluntresistance:number; pierceresistance: number; poisonresistance : number;}
+export type damageTypes = {heatdamage: number; energydamage:number; frostdamage:number; slashdamage: number; bluntdamage:number; piercedamage: number; poisondamage : number;}
+export type battleActionOutput = [Description[],string[]];
