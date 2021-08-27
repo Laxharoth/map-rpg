bfd5f67e8fd90f3dee486ec14df33dd6b85ca810
zeyden <zeydenlaxharoth@gmail.com>
2021-08-26 20:41:02 -0500

    Changed what triggers to allow specific triggers

--- src/app/classes/Character/Reaction/Reaction.ts
+++ src/app/classes/Character/Reaction/Reaction.ts
@@ -1,15 +1,23 @@
-import { battleActionOutput } from "src/app/customTypes/customTypes";
+import { ActionOutput } from "src/app/customTypes/customTypes";
 import { tag } from "src/app/customTypes/tags" 
+import { MasterService } from "../../masterService";
 import { Character } from "../Character";
 
 export abstract class Reaction{
-    protected abstract whatTriggers: tag[];
-    protected abstract action(source:Character,target: Character):battleActionOutput;
+    protected abstract whatTriggers: tag[][];
+    protected abstract action(source:Character,target: Character):ActionOutput;
+    protected masterService!:MasterService;
 
-    reaction(actionTags: tag[],source:Character,target:Character):battleActionOutput
+    constructor(masterService:MasterService)
+    { this.masterService = masterService; }
+
+    reaction(actionTags: tag[],source:Character,target:Character):ActionOutput
     {
-        if(actionTags.some(tag=>this.whatTriggers.includes(tag)))
-        { return this.action(source,target); }
+        for( const trigger of this.whatTriggers )
+        {
+            if(trigger.every(tag=>actionTags.includes(tag)))
+            { return this.action(source,target); }
+        }
         return [[],[]]
     }
 }
