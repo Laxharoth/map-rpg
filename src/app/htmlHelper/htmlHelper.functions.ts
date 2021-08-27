0f1bc993fd1f29e788edfc0ea6bc5bb48a5e6f61
zeyden <zeydenlaxharoth@gmail.com>
2021-08-26 21:15:07 -0500

    Added  helpers to create characterStats and weaponDamage pushBattleActionOutput returns the ActionOutputs

--- src/app/htmlHelper/htmlHelper.functions.ts
+++ src/app/htmlHelper/htmlHelper.functions.ts
@@ -1,5 +1,4 @@
-import { MasterService } from "../classes/masterService";
-import { battleActionOutput } from "../customTypes/customTypes";
+import { ActionOutput, characterStats, damageTypes } from "../customTypes/customTypes";
 
 export function getInputs():{input:string,select:string}
 {
@@ -10,10 +9,11 @@ export function getInputs():{input:string,select:string}
   return {input:inputValue, select:selectValue};
 }
 
-export function pushBattleActionOutput(source:battleActionOutput,target:battleActionOutput):void
+export function pushBattleActionOutput(source:ActionOutput,target:ActionOutput):ActionOutput
 {
   target[0].push(...source[0])
   target[1].push(...source[1])
+  return target;
 }
 
 export function randomBetween(min:number, max:number):number{
@@ -22,5 +22,38 @@ export function randomBetween(min:number, max:number):number{
 
 export function randomCheck(percent:number):boolean
 {
-  return percent < Math.random()*100;
+  return percent >= Math.random()*100;
 }
+
+export const loadCharacterStats = (function ()
+{
+  const _defaultStats:characterStats = { hitpoints : 1, energypoints : 0, 
+    attack : 0, aim: 0, defence : 0, speed : 0, evasion : 0,
+    heatresistance: 0, energyresistance:0, frostresistance:0, slashresistance: 0, bluntresistance:0, pierceresistance: 0, poisonresistance : 0,}
+  return function (characterStats:characterStats):characterStats
+  {
+    const myStats:characterStats = {..._defaultStats}
+    for(const key of Object.keys(characterStats))
+      myStats[key] = characterStats[key]
+    return myStats;
+  }
+})()
+
+export const loadWeaponDamage = (function() {
+  const _defaultStats:damageTypes = {
+     heatdamage  :0,
+     energydamage:0,
+     frostdamage :0,
+     slashdamage :0,
+     bluntdamage :0,
+     piercedamage:0,
+     poisondamage:0,
+  }
+  return function(weaponDamages:damageTypes):damageTypes
+  {
+    const myStats = {..._defaultStats}
+    for(const key of Object.keys(weaponDamages))
+      myStats[key] = weaponDamages[key]
+    return weaponDamages;
+  }
+})()
