0ea02a90547e0dadb56976598afac114c81ab29b
zeyden <zeydenlaxharoth@gmail.com>
2021-08-26 21:13:35 -0500

    Changed types that should be interfaces

--- src/app/customTypes/customTypes.ts
+++ src/app/customTypes/customTypes.ts
@@ -1,12 +1,12 @@
-import { Status } from "../classes/Character/Status/Status";
 import { Description } from "../classes/Descriptions/Description";
 import { Room } from "../classes/maps/room";
 import { MasterService } from "../classes/masterService";
 
 export type roomFunction = (masterService:MasterService) => Room;
-export type inputObject = {default: string , placeholder: string}
-export type characterStats = { hitpoints : number; energypoints : number; 
-                               attack : number; aim: number; defence : number; speed : number; evasion : number;
-                               heatresistance: number; energyresistance:number; frostresistance:number; slashresistance: number; bluntresistance:number; pierceresistance: number; poisonresistance : number;}
-export type damageTypes = {heatdamage: number; energydamage:number; frostdamage:number; slashdamage: number; bluntdamage:number; piercedamage: number; poisondamage : number;}
-export type battleActionOutput = [Description[],string[]];
+export interface inputObject {default: string , placeholder: string}
+export interface damageTypes {heatdamage?: number; energydamage?:number; frostdamage?:number; slashdamage?: number; bluntdamage?:number; piercedamage?: number; poisondamage? : number;}
+export type ActionOutput = [Description[],string[]];
+export interface characterStats { hitpoints ?: number; energypoints ?: number; 
+                               attack ?: number; aim?: number; defence ?: number; speed ?: number; evasion ?: number;
+                               heatresistance?: number; energyresistance?:number; frostresistance?:number; slashresistance?: number; bluntresistance?:number; pierceresistance?: number; poisonresistance ?: number;}
+
