8fecf7ed00e94f564cf4d696382088f102b3db74
zeyden <zeydenlaxharoth@gmail.com>
2021-08-26 21:13:54 -0500

    Created statusnames

---
+++ src/app/customTypes/statusnames.ts
@@ -0,0 +1,14 @@
+export type statusname = 'Defend' |
+ 'Paralisis' |
+ 'Poison' |
+ 'Ranged Attack' |
+ 'Blind' |
+ 'Charm' |
+ 'Fright' |
+ 'Grappled'|
+ 'Grappling'|
+ 'Invisible'|
+ 'Petrified'|
+ 'Prone'|
+ 'Restrained'|
+ 'Sleep';
