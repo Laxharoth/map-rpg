8ec8bd1fc4155ade4c14c0e0a49f7abac9430a3b
zeyden <zeydenlaxharoth@gmail.com>
2021-08-26 20:56:27 -0500

    Removed abstrac methods

--- src/app/classes/Character/Status/TimedStatus.ts
+++ src/app/classes/Character/Status/TimedStatus.ts
@@ -1,20 +1,7 @@
-import { battleActionOutput } from "src/app/customTypes/customTypes";
-import { Character } from "../Character";
 import { Status } from "./Status";
 
 export abstract class TimedStatus extends Status{
   protected abstract duration: number;
-  abstract get name(): string;
-  abstract get description(): string;
-  abstract onEffectEnded():battleActionOutput;
-  protected abstract effect(target: Character):battleActionOutput;
-  applyEffect(target: Character):battleActionOutput
-  {
-    const effect = this.effect(target);
-    return effect;
-  }
   get effectHasEnded():boolean
-  {
-    return this.duration<=0;
-  }
+  { return this.duration<=0; }
 }
