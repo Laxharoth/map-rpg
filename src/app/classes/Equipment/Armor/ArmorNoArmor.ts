de7fbc6949d56dcf6769dd84a567891a036ecd49
zeyden <zeydenlaxharoth@gmail.com>
2021-08-26 21:06:33 -0500

    Updated to have statModifiers

--- src/app/classes/Equipment/Armor/ArmorNoArmor.ts
+++ src/app/classes/Equipment/Armor/ArmorNoArmor.ts
@@ -1,13 +1,13 @@
-import { battleActionOutput } from "src/app/customTypes/customTypes";
+import { ActionOutput } from "src/app/customTypes/customTypes";
 import { tag } from "src/app/customTypes/tags";
 import { Character } from "../../Character/Character";
 import { Armor } from "./Armor";
 
 export class ArmorNoArmor extends Armor
 {
+    protected statsModifier = {}
     get name(): string { return 'No Armor'; }
     canEquip(character: Character): boolean { return false; }
-    applyModifiers(character: Character): void { }
     get tags(): tag[] { return ['no armor']; }
     get isSingleTarget(): boolean { return true; }
 }
