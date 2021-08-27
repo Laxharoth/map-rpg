379a2438839a5ded1fd8adddc3bc6bbb0b5e2832
zeyden <zeydenlaxharoth@gmail.com>
2021-08-26 21:07:03 -0500

    Added acuracyTest

--- src/app/classes/Equipment/Weapon/Melee/MeleeWeapon.ts
+++ src/app/classes/Equipment/Weapon/Melee/MeleeWeapon.ts
@@ -1,8 +1,16 @@
 import { Character } from "src/app/classes/Character/Character";
+import { randomBetween } from "src/app/htmlHelper/htmlHelper.functions";
 import { Weapon } from "../Weapon";
 
 export abstract class MeleeWeapon extends Weapon
 {
   protected damagestat(user   : Character):number{return user.stats.attack;}
   protected defencestat(target: Character):number{return target.stats.defence;}
+
+  protected accuracyTest(user:Character,target:Character)
+  {
+    let accuracyFix = 0;
+    if(target.hasTag('prone')) accuracyFix+=20;
+    return super.accuracyTest(user,target)+randomBetween(0,accuracyFix);
+  }
 }
--- src/app/classes/Equipment/Weapon/Ranged/RangedWeapon.ts
+++ src/app/classes/Equipment/Weapon/Ranged/RangedWeapon.ts
@@ -1,13 +1,14 @@
 import { Character } from "src/app/classes/Character/Character";
 import { StatusRangedAttack } from "src/app/classes/Character/Status/StatusTemporal/StatusRangedAttack";
-import { battleActionOutput } from "src/app/customTypes/customTypes";
+import { ActionOutput } from "src/app/customTypes/customTypes";
+import { randomBetween } from "src/app/htmlHelper/htmlHelper.functions";
 import { Weapon } from "../Weapon";
 
 export abstract class RangedWeapon extends Weapon
 {
   protected damagestat(user   : Character):number{return user.stats.aim;}
   protected defencestat(target: Character):number{return target.stats.defence;}
-  attack(user:Character,target:Character):battleActionOutput
+  attack(user:Character,target:Character):ActionOutput
   {
     const [descriptions,strings]=user.addStatus(new StatusRangedAttack(this.masterService));
     const [attackdescription,attackstring] =super.attack(user,target);
@@ -15,5 +16,12 @@ export abstract class RangedWeapon extends Weapon
     strings.push(...attackstring);
 
     return [descriptions,strings];
+  }
+
+  protected accuracyTest(user:Character,target:Character)
+  {
+    let accuracyFix = 0;
+    if(user.hasTag('restrained')) accuracyFix-=20;
+    return super.accuracyTest(user,target)+randomBetween(0,accuracyFix);
   }
 }
