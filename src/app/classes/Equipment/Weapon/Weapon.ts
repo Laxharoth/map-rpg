86fd448b38e663217f7e6e13f14790a4c7015551
zeyden <zeydenlaxharoth@gmail.com>
2021-08-26 21:09:10 -0500

    damage types is now initialized with helper function and is required in constructor (should be overriden so child does not require)

--- src/app/classes/Equipment/Weapon/Weapon.ts
+++ src/app/classes/Equipment/Weapon/Weapon.ts
@@ -1,19 +1,33 @@
-import { battleActionOutput, damageTypes } from './../../../customTypes/customTypes';
+import { ActionOutput, damageTypes } from './../../../customTypes/customTypes';
 import { Character } from "../../Character/Character";
 import { Equipment } from "../Equipment";
-import { randomBetween } from 'src/app/htmlHelper/htmlHelper.functions';
+import { loadWeaponDamage, pushBattleActionOutput, randomBetween } from 'src/app/htmlHelper/htmlHelper.functions';
+import { MasterService } from '../../masterService';
 
 export abstract class Weapon extends Equipment
 {
-  protected abstract damageTypes:damageTypes;
+  protected damageTypes:damageTypes;
   protected abstract accuracy:number;
-  attack(user:Character,target:Character):battleActionOutput
+  /**
+   * Should be overriden
+   * @param masterService should be added to the child contructor parameters
+   * @param damageTypes The damage stats of the weapon SHOULD NOT BE ADDED TO CHILD CONSTRUCTOR PARAMETERS
+   */
+  constructor(masterService:MasterService, damageTypes:damageTypes)
   {
-    if(this.accuracyTest(target) < 0 )
-    {return [[],[`${user.name} attacke ${target.name} but missed`]];}
+    super(masterService)
+    this.damageTypes = loadWeaponDamage(damageTypes)
+  }
+  attack(user:Character,target:Character):ActionOutput
+  {
+    if(this.accuracyTest(user,target) < 0 )
+    {return [[],[`${user.name} attack ${target.name} but missed`]];}
+    const [descriptions,strings]:ActionOutput = [[],[]];
     const damage = this.calculateDamage(user, target);
     target.stats.hitpoints-=damage;
-    return [[],[`${target.name} takes ${damage} damage from ${user.name}'s ${this.name}`]];
+    strings.push(`${target.name} takes ${damage} damage from ${user.name}'s ${this.name}`)
+    pushBattleActionOutput(target.react(this.tags,user),[descriptions,strings]);
+    return [descriptions,strings];
   }
   protected abstract damagestat(user   : Character):number;
   protected abstract defencestat(target: Character):number;
@@ -28,10 +42,13 @@ export abstract class Weapon extends Equipment
     finalDamage += (damageRelation * this.damageTypes.heatdamage||0   / (100 - target.roundStats.heatresistance));
     finalDamage += (damageRelation * this.damageTypes.energydamage||0 / (100 - target.roundStats.energyresistance));
     finalDamage += (damageRelation * this.damageTypes.frostdamage||0  / (100 - target.roundStats.frostresistance));
-    return Math.round(finalDamage);
+    return Math.round(finalDamage)||0;
   }
-  protected accuracyTest(target:Character): number {
-    const check = randomBetween(0,100+this.accuracy);
+  protected accuracyTest(user:Character,target:Character): number {
+    let [minaccuracy,maxaccuracy] = [0,100];
+    if(user.hasTag('blind')) maxaccuracy -= 20;
+    if(user.hasTag('aim')) maxaccuracy += 20;
+    const check = randomBetween(minaccuracy,maxaccuracy+this.accuracy);
     return check - target.roundStats.evasion;
   }
 }
