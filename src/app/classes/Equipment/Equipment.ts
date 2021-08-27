3bdedc1437ddb2942c928e29f50fe3b973a63a37
zeyden <zeydenlaxharoth@gmail.com>
2021-08-26 21:05:47 -0500

    Added reactions, specialAttacks ApplyModifiers is no longer abstract, uses stastModirier which is now characterStats

--- src/app/classes/Equipment/Equipment.ts
+++ src/app/classes/Equipment/Equipment.ts
@@ -1,20 +1,30 @@
-import { battleActionOutput } from "src/app/customTypes/customTypes";
+import { ActionOutput, characterStats } from "src/app/customTypes/customTypes";
 import { tag } from "src/app/customTypes/tags";
 import { Character } from "../Character/Character";
+import { Reaction } from "../Character/Reaction/Reaction";
 import { Item } from "../Items/Item";
+import { SpecialAttack } from "../Items/SpecialAttack/SpecialAttack";
 
 export abstract class Equipment extends Item
 {
   //Check if can equip
   abstract get name():string;
   abstract canEquip(character:Character ):boolean;
-  abstract applyModifiers(character:Character):void;
   abstract get tags():tag[];
-
+  protected readonly abstract statsModifier:characterStats;
+  
+  get reactions(): Reaction[]{return []};
+  get specials():SpecialAttack[]{return []};
+  
   get isBattleUsable(): boolean{return true;};
   get isPartyUsable(): boolean {return true;};
   get isEnemyUsable(): boolean {return false;};
   get isSelfUsableOnly(): boolean {return true;};
-
-  itemEffect(user:Character,target: Character): battleActionOutput { return [[],[]] }
+  
+  itemEffect(user:Character,target: Character): ActionOutput { return [[],[]] }
+  applyModifiers(character:Character):void
+  {
+    for(const key of Object.keys(this.statsModifier))
+    { character.stats[key] = this.statsModifier[key]}
+  }
 }
