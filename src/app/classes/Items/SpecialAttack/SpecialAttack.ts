506d71a844b4c582e8b65eb47f7c5db0dd353112
zeyden <zeydenlaxharoth@gmail.com>
2021-08-26 21:15:34 -0500

    disabled adapted to match parent

--- src/app/classes/Items/SpecialAttack/SpecialAttack.ts
+++ src/app/classes/Items/SpecialAttack/SpecialAttack.ts
@@ -1,9 +1,10 @@
+import { Character } from "../../Character/Character";
 import { Item } from "../Item";
 
 export abstract class SpecialAttack extends Item
 {
     cooldown: number = 0;
-    get disabled(): boolean { return this.cooldown > 0;}
+    disabled(user:Character): boolean { return this.cooldown > 0;}
 
     get isBattleUsable(): boolean { return true; }
     get isBattleUsableOnly(): boolean { return true; }
