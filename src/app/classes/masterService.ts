dea99bd48ba787a7fe6776e8049e773db0dec662
zeyden <zeydenlaxharoth@gmail.com>
2021-08-27 22:46:59 -0500

    Added party and enemyFormation Service Moved user and party to party Service

--- src/app/classes/masterService.ts
+++ src/app/classes/masterService.ts
@@ -1,7 +1,9 @@
 import { DescriptionHandlerService } from "../service/description-handler.service";
+import { EnemyFormationService } from "../service/enemy-formation.service";
 import { FlagHandlerService } from "../service/flag-handler.service";
 import { LockMapService } from "../service/lock-map.service";
 import { MapHandlerService } from "../service/map-handler.service";
+import { PartyService } from "../service/party.service";
 import { Character } from "./Character/Character";
 
 export class MasterService
@@ -10,23 +12,31 @@ export class MasterService
   private _descriptionHandler:DescriptionHandlerService
   private _flagsHandler:FlagHandlerService
   private _mapHandler:MapHandlerService
-  private _user:Character;
-  private _party: [(Character|null),(Character|null)] = [null,null];
+  private _partyHandler:PartyService
+  private _enemyHandler:EnemyFormationService
+
   constructor(savename:string)
   {
     this._lockmap = new LockMapService();
     this._descriptionHandler = new DescriptionHandlerService(this._lockmap);
     this._flagsHandler = new FlagHandlerService(savename);
     this._mapHandler = new MapHandlerService(this);
+    this._partyHandler = new PartyService();
+    this._enemyHandler = new EnemyFormationService();
   }
   get lockmap(){return this._lockmap;}
   get descriptionHandler(){return this._descriptionHandler;}
   get flagsHandler(){return this._flagsHandler;}
   get mapHandler(){return this._mapHandler;}
+  get partyHandler(){return this._partyHandler;}
+  get enemyHandler(){return this._enemyHandler;}
 
-  get user(){return this._user;}
-  get party():Character[]{return this._party.filter(character=> character!==null);}
-
-  set user(value:Character){this._user = value;}
-  setPartyMember(value:Character,index:number){if([0,1].includes(index))this._party[index] = value;}
+  updateCharacter(character:Character):void
+  {
+    if(character===this.partyHandler.user) return this.partyHandler.updateUser()
+    for(let partyIndeX = 0; partyIndeX < this.partyHandler.party.length; partyIndeX++)
+    if(this.partyHandler.party[partyIndeX]===character)return this.partyHandler.updatePartyMember(partyIndeX)
+    for(let enemyIndeX = 0; enemyIndeX < this.enemyHandler.enemyFormation.enemies.length; enemyIndeX++)
+    if(this.enemyHandler.enemyFormation.enemies[enemyIndeX]===character)return this.enemyHandler.updateEnemy(enemyIndeX)
+  }
 }
