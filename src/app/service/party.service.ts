7d256705f808cc64e726a15130e00100e6149821
zeyden <zeydenlaxharoth@gmail.com>
2021-08-29 18:40:56 -0500

    Added set to user and party member

--- src/app/service/party.service.ts
+++ src/app/service/party.service.ts
@@ -20,10 +20,10 @@ export class PartyService {
   get user(){return this._user;}
   get party():Character[]{return this._party.filter(character=> character!==null);}
 
-  set user(user:Character){this.user = user}
-  setPartyMember(value:Character,index:number){if([0,1].includes(index))this.party[index] = value;}
+  set user(user:Character){this._user = user; this.updateUser()}
+  setPartyMember(value:Character,index:number){if([0,1].includes(index))this.party[index] = value; this.updatePartyMember(index)}
 
-  updateUser() { this.userSubject.next(this._user); }
+  updateUser() { this.userSubject.next(this.user); }
   updatePartyMember(index:number) { this.partySubject.next(this._party); }
   onUpdateUser(): Observable<Character>  {return this.userSubject.asObservable();}
   onUpdateParty():Observable<Character[]>{return this.partySubject.asObservable();}
