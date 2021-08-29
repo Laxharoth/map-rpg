5a2b5f414d6a0502db8ab0e864076fe50a2baacd
zeyden <zeydenlaxharoth@gmail.com>
2021-08-29 18:57:55 -0500

    Added function  to decide if party can escape Added abstract fuction for escape success and fail Added abstract function for escapeCheck Added DescriptionOption to exit the battle.

--- src/app/classes/Character/NPC/EnemyFormations/EnemyFormation.ts
+++ src/app/classes/Character/NPC/EnemyFormations/EnemyFormation.ts
@@ -1,4 +1,4 @@
-import { Description } from "src/app/classes/Descriptions/Description";
+import { Description, DescriptionOptions } from "src/app/classes/Descriptions/Description";
 import { MasterService } from "src/app/classes/masterService";
 import { Character } from "../../Character";
 
@@ -12,4 +12,18 @@ export abstract class EnemyFormation
 
     abstract onEnemyVictory(party: Character[]):Description;
     abstract onPartyVictory(party: Character[]):Description;
+    attemptEscape(party: Character[]):Description
+    {
+      if(this.escapeCheck(party))
+      {
+        for(const character of party){character.onEndBattle();}
+        return this.escapeSuccess();
+      }
+      return this.escapeFail();
+    }
+
+    protected abstract escapeSuccess():Description;
+    protected abstract escapeFail():Description;
+    protected abstract escapeCheck(party: Character[]):boolean;
+    protected exitOption(exitString:string):DescriptionOptions {return new DescriptionOptions(exitString,()=>{ this.masterService.descriptionHandler.flush(0).setDescription(false)})}
 }
