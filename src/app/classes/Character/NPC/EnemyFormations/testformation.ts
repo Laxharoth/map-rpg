359b7b53c1b57741b3f38c813ab52b3f8cd99248
zeyden <zeydenlaxharoth@gmail.com>
2021-08-29 19:00:23 -0500

    Implemented escape abstract functions

--- src/app/classes/Character/NPC/EnemyFormations/testformation.ts
+++ src/app/classes/Character/NPC/EnemyFormations/testformation.ts
@@ -10,8 +10,26 @@ export class testformation extends EnemyFormation
     constructor(masterService:MasterService)
     {
         super(masterService)
-        this._enemies = Array.from(Array(randomBetween(1,3))).map(_=>new enemyTest(this.masterService))
+        //this._enemies = Array.from(Array(randomBetween(1,3))).map(_=>new enemyTest(this.masterService))
+        this._enemies = [new enemyTest(this.masterService)]
     }
+
+    get descriptionMessage():string {return `${this.masterService.partyHandler.user.name} escapes`}
+
+    protected escapeSuccess():Description
+    {
+      const nextOption = this.exitOption("Exit");
+      return new Description(()=>this.descriptionMessage,[nextOption]);
+    }
+    protected escapeFail():Description
+    {
+      throw Error("");
+    }
+    protected escapeCheck(party: Character[]):boolean
+    {
+      return true;
+    }
+
 
     protected _enemies: Character[];
     onEnemyVictory(party: Character[]): Description {
@@ -20,14 +38,14 @@ export class testformation extends EnemyFormation
     onPartyVictory(party: Character[]): Description {
         return this.partyVictory(party)
     }
-    
+
     //////////////////////////
     // Enemy Victory
     //////////////////////////
     private enemyVictory(party: Character[]): Description {
         const options = [new DescriptionOptions('next',()=>{
-            party.forEach(char=>{char.stats.hitpoints = char.originalstats.hitpoints})
-            this.masterService.descriptionHandler.nextDescription(false) 
+            party.forEach(char=>{char.healHitPoints(Infinity)})
+            this.masterService.descriptionHandler.nextDescription(false)
         })]
         return new Description(()=>`Enemy won`, options)
     }
@@ -36,8 +54,8 @@ export class testformation extends EnemyFormation
     //////////////////////////
     private partyVictory(party: Character[]): Description {
         const options = [new DescriptionOptions('next',()=>{
-            party.forEach(char=>{char.stats.hitpoints = Math.min(char.stats.hitpoints+10,char.originalstats.hitpoints)})
-            this.masterService.descriptionHandler.nextDescription(false) 
+            party.forEach(char=>{char.healHitPoints(10)})
+            this.masterService.descriptionHandler.nextDescription(false)
         })]
         return new Description(()=>`Party won`, options)
     }
