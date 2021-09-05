8a4433d3a1b001d76eca98c1fde105fc7e3f145e
zeyden <zeydenlaxharoth@gmail.com>
2021-09-04 22:02:27 -0500

    Added function to get character altered status

--- src/app/component/party-member/party-member.component.ts
+++ src/app/component/party-member/party-member.component.ts
@@ -1,5 +1,6 @@
 import { Character } from 'src/app/classes/Character/Character';
 import { Component, OnInit, Input } from '@angular/core';
+import { Status } from 'src/app/classes/Character/Status/Status';
 
 @Component({
   selector: 'app-party-member',
@@ -9,9 +10,13 @@ import { Component, OnInit, Input } from '@angular/core';
 export class PartyMemberComponent implements OnInit {
 
   @Input() partyMember:Character;
-
   constructor() { }
 
   ngOnInit(): void { }
 
+  getCharacterStatus():Status[] {
+    const status:Status[] = [];
+    for(const characterStatus of this.partyMember.iterStatus())status.push(characterStatus)
+    return status;
+  }
 }
