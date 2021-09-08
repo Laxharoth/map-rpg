fbb39ca6335169136b36fc498d7289aff131d37b
zeyden <zeydenlaxharoth@gmail.com>
2021-09-07 21:30:46 -0500

    Timed Status removes itself

--- src/app/classes/Character/Status/TimedStatus.ts
+++ src/app/classes/Character/Status/TimedStatus.ts
@@ -1,7 +1,44 @@
+import { Character } from 'src/app/classes/Character/Character';
+import { Subscription } from 'rxjs';
 import { Status } from "./Status";
+import { MasterService } from '../../masterService';
+import { Time } from '../../Time';
 
 export abstract class TimedStatus extends Status{
   protected abstract duration: number;
+  protected initialTime: number;
+  protected currentTime: number;
+  protected timerSubscription:Subscription;
+  constructor(masterService:MasterService)
+  {
+    super(masterService);
+    this.initialTime = this.masterService.flagsHandler.minutes;
+    this.currentTime = this.initialTime;
+  }
   get effectHasEnded():boolean
-  { return this.duration<=0; }
+  { return this.remainingTime <= 0; }
+  get remainingTime(): number
+  { return this.duration - (this.currentTime - this.initialTime); }
+  onStatusGainded(target: Character)
+  {
+    this.timerSubscription = this.masterService.flagsHandler.onTimeChanged().subscribe( time => {
+      this.checkRemoveStatus(time, target);
+    })
+    return super.onStatusGainded(target)
+  }
+
+  private checkRemoveStatus(time: Time, target: Character) {
+    this.currentTime = time.getMinutes();
+    if (this.effectHasEnded) {
+      const [message] = target.removeStatus(this);
+      this.masterService.descriptionHandler.tailDescription(...message);
+    }
+  }
+
+  onStatusRemoved(target: Character)
+  {
+    this.timerSubscription.unsubscribe()
+    return super.onStatusRemoved(target)
+  }
+
 }
