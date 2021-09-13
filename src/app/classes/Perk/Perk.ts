66248a37a11d36ced70d2d0fbdc84c0a0c8503df
zeyden <zeydenlaxharoth@gmail.com>
2021-09-13 11:48:06 -0500

    implemented storeable

--- src/app/classes/Perk/Perk.ts
+++ src/app/classes/Perk/Perk.ts
@@ -1,10 +1,11 @@
+import { storeable } from "src/app/customTypes/customTypes";
 import { perkname } from "src/app/customTypes/perkname";
 import { tag } from "src/app/customTypes/tags";
 import { Reaction } from "../Character/Reaction/Reaction";
 import { SpecialAttack } from "../Items/SpecialAttack/SpecialAttack";
 import { MasterService } from "../masterService";
 
-export abstract class Perk
+export abstract class Perk implements storeable
 {
     abstract get name():perkname;
     protected readonly masterService:MasterService;
