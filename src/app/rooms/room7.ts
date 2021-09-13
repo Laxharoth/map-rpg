006a16a9850123bf78f40003878560d4f149bf8f
zeyden <zeydenlaxharoth@gmail.com>
2021-09-13 11:07:58 -0500

    Added options to test equip items

--- src/app/rooms/room7.ts
+++ src/app/rooms/room7.ts
@@ -1,8 +1,10 @@
-import { charTest } from "../classes/Character/NPC/characterTest";
+import { ArmorNoArmor } from './../classes/Equipment/Armor/ArmorNoArmor';
 import { testformation } from "../classes/Character/NPC/EnemyFormations/testformation";
-import { enemyTest } from "../classes/Character/NPC/enemyTest";
 import { Description, DescriptionOptions } from "../classes/Descriptions/Description";
-import { descriptionFight } from "../classes/Descriptions/DescriptionFight";
+import { descriptionBattle } from "../classes/Descriptions/BattleDescription";
+import { ShieldNoShield } from "../classes/Equipment/Shield/ShieldNoShield";
+import { MeleeUnharmed } from "../classes/Equipment/Weapon/Melee/MeleeUnharmed";
+import { RangedUnharmed } from "../classes/Equipment/Weapon/Ranged/RangedUnharmed";
 import { Room } from "../classes/maps/room";
 import { MasterService } from "../classes/masterService";
 
@@ -10,14 +12,71 @@ export function room(masterService:MasterService):Room
 {
   const roomName = 'room7'
   const $flag = (name:string) => masterService.flagsHandler.getFlag(name);
+  const user = masterService.partyHandler.user;
+  let melee = user.inventary[0];
+  let ranged = user.inventary[1];
+  let shield = user.inventary[2];
+  let armor = user.inventary[3];
+  const equipMelee = new DescriptionOptions(user.meleeWeapon instanceof MeleeUnharmed?'Equip Melee':"Unequip",function(){
+    if(user.meleeWeapon instanceof MeleeUnharmed)
+    {
+      user.useItem(melee,[user]);
+      this.text='Unequip'
+    }
+    else
+    {
+      user.unequipMelee();
+      this.text='Equip Melee'
+    }
+    console.log(user)
+  })
+  const equipRanged = new DescriptionOptions(user.rangedWeapon instanceof RangedUnharmed?'Equip ranged':"Unequip",function(){
+    if(user.rangedWeapon instanceof RangedUnharmed)
+    {
+      user.useItem(ranged,[user]);
+      this.text='Unequip'
+    }
+    else
+    {
+      user.unequipRanged();
+      this.text='Equip ranged'
+    }
+    console.log(user)
+  })
+  const equipShield = new DescriptionOptions(user.shield instanceof ShieldNoShield?'Equip Shield':"Unequip",function(){
+    if(user.shield instanceof ShieldNoShield)
+    {
+      user.useItem(shield,[user]);
+      this.text='Unequip'
+    }
+    else
+    {
+      user.unequipShield();
+      this.text='Equip Shield'
+    }
+    console.log(user)
+  })
+  const equipArmor = new DescriptionOptions(user.armor instanceof ArmorNoArmor?'Equip Armor':"Unequip",function(){
+    if(user.armor instanceof ArmorNoArmor)
+    {
+      user.useItem(armor,[user]);
+      this.text='Unequip'
+    }
+    else
+    {
+      user.unequipArmor();
+      this.text='Equip Armor'
+    }
+    console.log(user)
+  })
   const nextOption      = new DescriptionOptions("next",function(){masterService.descriptionHandler.nextDescription()});
   const roomOptions =[
     new DescriptionOptions("option1",function(){masterService.flagsHandler.setFlag("",0)}),
-    new DescriptionOptions("test battle",function(){ descriptionFight(masterService,new testformation(masterService) ) }),
-    new DescriptionOptions("option2",function(){}),
-    new DescriptionOptions("option2",function(){}),
-    new DescriptionOptions("option2",function(){}),
-    new DescriptionOptions("option2",function(){}),
+    new DescriptionOptions("test battle",function(){ descriptionBattle(masterService,new testformation(masterService) ) }),
+    equipMelee,
+    equipRanged,
+    equipShield,
+    equipArmor,
     new DescriptionOptions("option2",function(){}),
     new DescriptionOptions("option2",function(){}),
     new DescriptionOptions("option2",function(){}),
