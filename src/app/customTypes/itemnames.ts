a5193ead19a1243f78cb2ae721949490015af1a0
zeyden <zeydenlaxharoth@gmail.com>
2021-09-17 17:26:00 -0500

    Renamed file Added enums for names of each kind of item

---
+++ src/app/customTypes/itemnames.ts
@@ -0,0 +1,33 @@
+export enum meleenameEnum {
+  "hand"="hand",
+  "Melee test"="Melee test",
+};
+export enum rangednameEnum{
+  "a rock"="a rock",
+  "Ranged Test"="Ranged Test",
+}
+export enum shieldnameEnum{
+  "No shield"="No shield",
+  "Shield test"="Shield test",
+}
+export enum armornameEnum{
+  "No Armor"="No Armor",
+  "Armor Test"="Armor Test",
+}
+export enum specialsnameEnum{
+  'One Punch'='One Punch',
+  'Charm'='Charm',
+  'Fright'='Fright',
+  'Grab'='Grab',
+}
+export enum itemsEnum{
+  'item-test'='item-test',
+}
+export type meleename = `${meleenameEnum}`;
+export type rangedname =`${rangednameEnum}`;
+export type shieldname =`${shieldnameEnum}`;
+export type armorname = `${armornameEnum}`;
+export type weaponname = meleename|rangedname;
+export type equipmentname = weaponname| shieldname| armorname;
+export type specialsname = `${specialsnameEnum}`;
+export type itemname = equipmentname|specialsname|`${itemsEnum}`;
