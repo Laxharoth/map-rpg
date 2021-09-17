86462e6d8dac7686bd542711f619dfe0271696d2
zeyden <zeydenlaxharoth@gmail.com>
2021-09-17 17:09:07 -0500

    Created test for persistentCharacter

---
+++ src/app/classes/Character/NPC/JohnSmit.ts
@@ -0,0 +1,18 @@
+import { MasterService } from 'src/app/classes/masterService';
+import { characterType } from "src/app/customTypes/characterTypes";
+import { ActionOutput } from "src/app/customTypes/customTypes";
+import { Character } from "../Character";
+import { PersistentCharacter } from "./PersistentCharacter";
+
+export class JohnSmith extends PersistentCharacter
+{
+  protected _name: string = "John Smith";
+  characterType: characterType= 'john';
+  constructor(masterService:MasterService)
+  {
+    super({},masterService);
+  }
+  IA_Action(ally: Character[], enemy: Character[]): ActionOutput {
+    throw new Error("Method not implemented.");
+  }
+}
