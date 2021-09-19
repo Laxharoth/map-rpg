a42792f4c97dcf818058b278bc6e3f70553be08a
zeyden <zeydenlaxharoth@gmail.com>
2021-09-18 21:23:27 -0500

    renamed descriptionList _descriptionList and changed to object Added attribute gameStateHandler added get descriptionList to retrieve the "descriptionList" with the gameState from gameStateService Added private function addDescriptionListWithGameState Set description will check if current descriptionList has descriptions (or exists) and pops gameStates until finds a list with descriptions inserting description will try to set a game_state, then will insert the descriptions into the correct descriptionList removed flush function

--- src/app/service/description-handler.service.ts
+++ src/app/service/description-handler.service.ts
@@ -3,25 +3,37 @@ import { Observable, Subject } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { DoubleLinkedList, DoubleLinkedListNode } from './../classes/DoubleLinkedList';
 import { LockMapService } from './lock-map.service';
+import { game_state } from '../customTypes/states';
+import { GameStateService } from './game-state.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DescriptionHandlerService {
-
-  private descriptionList = new DoubleLinkedList<Description>();
+  private _descriptionList:{[key: string]:DoubleLinkedList<Description>}={};
   private descriptionTextHistory = new DoubleLinkedList<string>();
   private pivot:DoubleLinkedListNode<string>;
   private descriptionSubject = new Subject<Description>();
   private descriptionTextSubject = new Subject<string>();
   private lockmap:LockMapService;
-
-  constructor(lockmap:LockMapService){
-    this.descriptionList.insertHead(null);
-    this.lockmap = lockmap
+  private gameStateHandler:GameStateService;
+  private get descriptionList():DoubleLinkedList<Description>
+  {return this._descriptionList[this.currentGameState]}
+  constructor(lockmap:LockMapService,gameStateHandler:GameStateService){
+    this.lockmap = lockmap;
+    this.gameStateHandler=gameStateHandler;
+    this.addDescriptionListWithGameState('map');
+    this._descriptionList['map'].insertHead(null);
+  }
+  private addDescriptionListWithGameState(gameState: string) {
+    if(!this._descriptionList[gameState])
+      this._descriptionList[gameState] = new DoubleLinkedList<Description>();
   }
 
-  setDescription(addToHistory:boolean=true):DescriptionHandlerService{
+  private get currentGameState() { return this.gameStateHandler.gameState; }
+  setDescription(addToHistory:boolean=true):DescriptionHandlerService
+  {
+    while(!this.descriptionList || this.descriptionList.length === 0){ this.gameStateHandler.popState(); }
     if(this.descriptionList.length>1)this.lockmap.lockMap("description-lock");
     else this.lockmap.unlockMap("description-lock");
 
@@ -34,37 +46,34 @@ export class DescriptionHandlerService {
     this.descriptionTextSubject.next(this.descriptionList.head.value.text());
     return this
   }
-
-  headDescription(...description:Description[]):DescriptionHandlerService{
-    this.descriptionList.insertHead(...description);
+  headDescription(description:Description|Description[],gameState:game_state):DescriptionHandlerService{
+    this.gameStateHandler.gameState = gameState;
+    if(description instanceof Description)description = [description]
+    this.addDescriptionListWithGameState(gameState);
+    this._descriptionList[gameState].insertHead(...description);
     return this
   }
 
-  afterHeadDescription(...description:Description[]):DescriptionHandlerService{
-    this.descriptionList.insertBefore(1,...description);
+  afterHeadDescription(description:Description|Description[],gameState:game_state):DescriptionHandlerService{
+    this.gameStateHandler.gameState = gameState;
+    if(description instanceof Description)description = [description]
+    this.addDescriptionListWithGameState(gameState);
+    this._descriptionList[gameState].insertBefore(1,...description);
     return this
   }
 
-  tailDescription(...description:Description[]):DescriptionHandlerService{
-    this.descriptionList.insertTail(...description);
+  tailDescription(description:Description|Description[],gameState:game_state):DescriptionHandlerService{
+    this.gameStateHandler.gameState = gameState;
+    if(description instanceof Description)description = [description]
+    this.addDescriptionListWithGameState(gameState);
+    this._descriptionList[gameState].insertTail(...description);
     return this
   }
 
   nextDescription(addToHistory:boolean=true):DescriptionHandlerService{
-    if(this.descriptionList.length > 1)
-      this.descriptionList.removeAt(0);
-    if(this.descriptionList.length > 0){
-      this.setDescription(addToHistory);
-    }
+    this.descriptionList?.removeAt(0);
+    this.setDescription(addToHistory);
     return this
-  }
-
-  flush(descriptionNumber: number):DescriptionHandlerService
-  {
-    while(this.descriptionList.length>descriptionNumber+1)
-    { this.descriptionList.removeAt(0); }
-    this.setDescription(false);
-    return this;
   }
 
   onSetDescription():Observable<Description>
