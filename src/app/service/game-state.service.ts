949a00ef67e6ffccfb07a051a117f4ec970502a5
zeyden <zeydenlaxharoth@gmail.com>
2021-09-18 21:16:39 -0500

    Added _gameStatePriority and _gameStateIndex _gameState changed for array _gameState initialized in constructor with first in priority updated get gameState to return the last game_state in _gameState update set gameState to insert the game_states in the order by priority until it is the one in the argument. Added function to pop from _gameState

--- src/app/service/game-state.service.ts
+++ src/app/service/game-state.service.ts
@@ -8,17 +8,30 @@ import { game_state } from '../customTypes/states';
 export class GameStateService {
 
   private gameStateSubject = new Subject<game_state>();
-  private _gameState:game_state = 'map';
+  private _gameStatePriority:game_state[] = ['map','item','battle'];
+  private _gameState:game_state[] = [];
+  private _gameStateIndex:number = 0;
+  constructor() {this.gameState = this._gameStatePriority[0]; }
 
-  constructor() { }
+  set gameStatePriority(state:game_state[]){ this._gameStatePriority = state; }
 
-  set gameState(state:game_state){
-    this._gameState = state;
-    this.gameStateSubject.next(this._gameState);
+  get gameState():game_state { return this._gameState[this._gameState.length - 1]; }
+  set gameState(state:game_state)
+  {
+    const pushStateInPriority=()=>{
+      this._gameState.push(this._gameStatePriority[this._gameStateIndex]);
+      this._gameStateIndex++;
+      this.gameStateSubject.next(this.gameState);
+    }
+    if(this._gameState.includes(state))return;
+    while(this.gameState!==state){pushStateInPriority();}
   }
-
-  get gameState():game_state { return this._gameState;}
-
+  popState()
+  {
+    this._gameState.pop();
+    this._gameStateIndex--;
+    this.gameStateSubject.next(this.gameState)
+  }
   onSetGameState():Observable<game_state> {
     return this.gameStateSubject.asObservable();
   }
