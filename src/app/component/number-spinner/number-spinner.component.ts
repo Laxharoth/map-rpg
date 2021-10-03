c82fe92d9dbccf86e92ca77174bbce7123e4e768
zeyden <zeydenlaxharoth@gmail.com>
2021-10-03 18:36:35 -0500

    Added number input component (number-spinner)

---
+++ src/app/component/number-spinner/number-spinner.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit, Output, EventEmitter, ViewChild, ElementRef, Input } from '@angular/core';
+import * as jQuery from 'jquery';
+
+@Component({
+  selector: 'app-number-spinner',
+  templateUrl: './number-spinner.component.html',
+  styleUrls: ['./number-spinner.component.css']
+})
+export class NumberSpinnerComponent implements OnInit {
+
+  @Input() spinnerType:'horizontal'|'vertical'='horizontal';
+  @Input() initialValue:number;
+  @Output() SpinnerChangedEvent = new EventEmitter<number>();
+  @ViewChild('InputValue') inputValue: ElementRef;
+  constructor() { }
+
+  ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    jQuery(this.inputValue.nativeElement)
+      .val(this.initialValue)
+      .on('change',()=>{this.emmitChange()});
+  }
+  changeInput(change:number)
+  {
+    const $input = jQuery(this.inputValue.nativeElement)
+    const currentVal = Number.parseInt($input.val() as string);
+    const changedValue = currentVal + change
+    $input.val(changedValue)
+          .trigger('change');
+  }
+  emmitChange()
+  {
+    const $input = jQuery(this.inputValue.nativeElement)
+    const min = Number.parseFloat($input.attr('min'));
+    const max = Number.parseFloat($input.attr('max'));
+    $input.val(Math.max(min,this.InputValue))
+          .val(Math.min(max,this.InputValue))
+    this.SpinnerChangedEvent.emit(this.InputValue);
+  }
+  private get InputValue():number
+  {
+    const $input = jQuery(this.inputValue.nativeElement)
+    return Number.parseInt($input.val() as string) || this.initialValue;
+  }
+}
