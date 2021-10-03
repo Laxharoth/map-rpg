76650a6bc28b59a1917a16535895c0f1f1344aab
zeyden <zeydenlaxharoth@gmail.com>
2021-10-03 18:37:21 -0500

    Removed unused imports

--- src/app/component/tab-menu/tab-menu.component.ts
+++ src/app/component/tab-menu/tab-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { MasterService } from 'src/app/classes/masterService';
 
 @Component({
