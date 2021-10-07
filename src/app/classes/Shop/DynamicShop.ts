ac580b80e17215de4b8d0a45d6b103121f3b09d6
zeyden <zeydenlaxharoth@gmail.com>
2021-10-06 20:53:07 -0500

    -Added shopPrices variable. -Implemented storeable interface functions.

--- src/app/classes/Shop/DynamicShop.ts
+++ src/app/classes/Shop/DynamicShop.ts
@@ -1,22 +1,36 @@
-import { storeable } from 'src/app/customTypes/customTypes';
 import { itemname } from 'src/app/customTypes/itemnames';
+import { storeable } from 'src/app/customTypes/customTypes';
 import { MasterService } from "src/app/service/master.service";
+import { ItemFactory } from '../Character/Factory/ItemFactory';
 import { Shop } from "./Shop";
 
 export class DynamicShop extends Shop implements storeable{
 
-  constructor(name:string,shopItemNames:itemname[],masterService: MasterService,shopPrices)
+  private shopPrices:{[key:string]:number};
+  constructor(name:string,masterService: MasterService,shopPrices:{[key:string]:number}={})
   {
     //itemas are added from json
-    super(name,[],masterService,shopPrices)
+    super(name,[],masterService,{})
+    this.shopPrices = shopPrices
   }
   // TODO save the shop inventary
-  toJson(): { [key: string]: any; } {
-    throw new Error('Method not implemented.');
+  toJson(): {[key: string]:{options:{amount:number}} } {
+    const shopSavedata:{[key: string]:{options:{amount:number}} } = {}
+    for(const item of this.shopItems)
+    {
+      if(item.name in shopSavedata){ shopSavedata[item.name].options.amount += item.amount;continue;}
+      shopSavedata[item.name] = {options:item.toJson()};
+    }
+    return shopSavedata
   }
   //TODO load the shop inventary
-  fromJson(options: { [key: string]: any; }): void {
-    throw new Error('Method not implemented.');
+  fromJson(options: {[key: string]:{options:{amount:number}} }): void {
+    for(const [key,{options:itemOptions}] of Object.entries(options))
+    {
+      const item = ItemFactory(this.masterService,key as itemname,itemOptions);
+      if(key in this.shopPrices) { item.basePrice = this.shopPrices[key] }
+      this.addItem(item);
+    }
   }
 
 }
