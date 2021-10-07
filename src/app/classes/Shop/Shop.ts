f477fdf8eecf852acad431d757115ec57541257b
zeyden <zeydenlaxharoth@gmail.com>
2021-10-06 20:44:30 -0500

    -Added function to check if character inventary will overflow -Added function to addItem to shop -Added function to move items sold by the player to the shop and bought by the player to the player. -Added function to put all the items of a kind in a single Object to check if the inventary will overflow.

--- src/app/classes/Shop/Shop.ts
+++ src/app/classes/Shop/Shop.ts
@@ -6,12 +6,12 @@ import { Character } from '../Character/Character';
 import { Sale } from './Sale';
 
 //TODO Shop must be generic, only changes Items and maybe price.
-//TODO Check if inventary is full or not.
-//TODO function to finish the sale
 export class Shop
 {
   private _name: string;
-  private masterService:MasterService;
+  private calculatedPlayerOverflow = false;
+  private currectPlayerOverflow = false;
+  protected masterService:MasterService;
   get name():string { return this._name; }
   shopItems:Item[]=[];
   sale = new Sale();
@@ -30,17 +30,21 @@ export class Shop
   shopSellItem(item:Item,amount:number){
     const shopItem:Item = Object.create(item);
     shopItem.amount = amount;
+    this.calculatedPlayerOverflow = false;
     this.sale.addItem2Character(shopItem)
   }
   shopBuyItem(item:Item,amount:number){
+    this.calculatedPlayerOverflow = false;
     const itemToMove:Item = Object.create(item);
     itemToMove.amount = Math.min(amount,item.amount);
     this.sale.addItem2Shop(itemToMove);
   }
   shopRemoveSellItem(item:Item,amount:number){
+    this.calculatedPlayerOverflow = false;
     this.sale.removeItem2Character(item,amount);
   }
   shopRemoveBuyItem(item:Item,amount:number){
+    this.calculatedPlayerOverflow = false;
     this.sale.removeItem2Shop(item,amount);
   }
 
@@ -79,5 +83,86 @@ export class Shop
       inventory.push(copy);
     }
     return inventory;
+  }
+  addItem(item:Item):void
+  {
+    if(!item){console.warn("Item not found, Is null or undefined."); return;}
+    this.fitItemIntoInventary(item);
+    if(item.amount <= 0) return;
+    this.shopItems.push(item);
+  }
+  CheckoutSale(character:Character):void
+  {
+    this.CheckoutSaleUpdateCharacterItems(character);
+    this.CheckoutSaleUpdateShopInventory();
+    //Reset sale items
+    this.sale.items2Shop = [];
+    this.sale.items2Character =[];
+  }
+  private CheckoutSaleUpdateCharacterItems(character: Character) {
+    const copyItemsAmount: { [key: string]: number; } = {};
+    for (const item of this.sale.items2Shop) { copyItemsAmount[item.name] = item.amount; }
+    for (const characteritem of character.inventary) {
+      if (copyItemsAmount.hasOwnProperty(characteritem.name)) {
+        const reduceAmount = Math.min(characteritem.amount, copyItemsAmount[characteritem.name]);
+        characteritem.amount -= reduceAmount;
+        copyItemsAmount[characteritem.name] -= reduceAmount;
+      }
+    }
+    for (const item of this.sale.items2Character) character.addItem(item);
+    character.originalstats.gold-=this.sale.total;
+  }
+  private CheckoutSaleUpdateShopInventory() {
+    const shopInventoryAfterSale = this.shopInventoryAfterSale;
+    this.shopItems = shopInventoryAfterSale;
+    for (const item of this.sale.items2Shop)
+      this.addItem(item);
+  }
+
+  doesCharacterInventoryOverflows(character: Character):boolean
+  {
+    if(!this.calculatedPlayerOverflow)
+    {
+      this.calculatedPlayerOverflow = true;
+      this.currectPlayerOverflow =  character.inventarysize
+                                    <
+                                    this.mergeCharacterItems(character)
+                                      .reduce((acc, item) => acc + Math.ceil(item.amount/item.maxStack),0);
+    }
+    return this.currectPlayerOverflow;
+  }
+  private fitItemIntoInventary(item: Item):void
+  {
+    if(item.amount<=0)return;
+    for (const shopItem of this.shopItems)
+      if (shopItem.constructor === item.constructor)
+      {
+        shopItem.amount += item.amount;
+        item.amount = 0;
+        return;
+      }
+  }
+  private mergeCharacterItems(character: Character):Item[]
+  {
+    const items:Item[] = [];
+    const characterInventoryAfterSale = this.sale.items2Character.concat(this.getCharacterInvetoryAfterSale(character));
+
+    for(const characterItem of characterInventoryAfterSale)
+    {
+      let addItem = true;
+      const copy =  Object.create(characterItem);
+      for(const item of items)
+      {
+        if(copy.constructor === item.constructor)
+        {
+          item.amount += copy.amount;
+          addItem = false;
+          break;
+        }
+      }
+      if(addItem)items.push(copy);
+    }
+
+    return items;
   }
 }
