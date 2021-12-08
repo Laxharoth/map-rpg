687fab2204a0e348001ea7a61afa443a90e76704
zeyden <zeydenlaxharoth@gmail.com>
2021-12-07 21:31:02 -0600

    Moved functions to the end of the class

--- src/gameLogic/custom/Class/maps/room.ts
+++ src/gameLogic/custom/Class/maps/room.ts
@@ -7,34 +7,6 @@ import { MasterService } from "src/app/service/master.service";
  * @class Room
  */
 export class Room{
-  /**
-   * The action to be performed when the room is entered.
-   *
-   * @memberof Room
-   */
-  onEnter     : () => void;
-  /**
-   * The action to be performed when the room is exit.
-   *
-   * @memberof Room
-   */
-  onExit      : () => void;
-  /**
-   * Perform an action before moving to a room.
-   *
-   * @param {string} roomName The name of the room to be moved.
-   * @returns true if can move to the specified room.
-   * @memberof Room
-   */
-  beforeMoveTo: (roomName:string) => boolean;
-  /**
-   * Perform an action after moving to a room.
-   *
-   * @param {string} roomName The name of the room to be moved.
-   * @memberof Room
-   */
-  afterMoveTo: (roomName:string) => void;
-  icon        : string;
   constructor({onEnter,onExit,beforeMoveTo=(roomName:string) => true,afterMoveTo=(roomName:string)=>{},icon}) {
     this.onEnter       = onEnter;
     this.onExit        = onExit;
@@ -42,6 +14,54 @@ export class Room{
     this.afterMoveTo   = afterMoveTo;
     this.icon          = icon;
   }
+    /**
+   * The action to be performed when the room is entered.
+   * * Room functions order:
+   *  * onEnter
+   *  * beforeMoveTo
+   *  * onExit
+   *  * afterMoveTo
+   *
+   * @memberof Room
+   */
+     onEnter     : () => void;
+     /**
+      * The action to be performed when the room is exited.
+      * * Room functions order:
+      *   * onEnter
+      *   * beforeMoveTo
+      *   * onExit
+      *   * afterMoveTo
+      *
+      * @memberof Room
+      */
+     onExit      : () => void;
+     /**
+      * Perform an action before moving to a room.
+      * * Room functions order:
+      *   * onEnter
+      *   * beforeMoveTo
+      *   * onExit
+      *   * afterMoveTo
+      *
+      * @param {string} roomName The name of the room to be moved.
+      * @returns true if can move to the specified room.
+      * @memberof Room
+      */
+     beforeMoveTo: (roomName:string) => boolean;
+     /**
+      * Perform an action after moving to a room.
+      * * Room functions order:
+      *   * onEnter
+      *   * beforeMoveTo
+      *   * onExit
+      *   * afterMoveTo
+      *
+      * @param {string} roomName The name of the room to be moved.
+      * @memberof Room
+      */
+     afterMoveTo: (roomName:string) => void;
+     icon        : string;
 }
 
 export type roomFunction = (masterService:MasterService) => Room;
