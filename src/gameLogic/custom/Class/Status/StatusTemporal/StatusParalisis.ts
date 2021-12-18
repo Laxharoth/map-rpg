3a33e788b70f8774ea5184c93564fc6d2cbaef0c
zeyden <zeydenlaxharoth@gmail.com>
2021-12-17 19:47:39 -0600

    Updated to new stats naming

--- src/gameLogic/custom/Class/Status/StatusTemporal/StatusParalisis.ts
+++ src/gameLogic/custom/Class/Status/StatusTemporal/StatusParalisis.ts
@@ -12,8 +12,7 @@ export class StatusParalisis extends StatusBattle {
   get description(): string {
     return "The target can't move due to an energy shock.";
   }
-  protected effect(target: Character): ActionOutput
-  { return [[],[`${target.name} can't move`]] }
+  protected effect(target: Character): ActionOutput { return [[],[`${target.name} can't move`]] }
   onStatusGainded(target: Character): ActionOutput
   {
     switch(Math.floor(Math.random() * 7))
@@ -30,6 +29,6 @@ export class StatusParalisis extends StatusBattle {
   onStatusRemoved(target: Character): ActionOutput
   { return pushBattleActionOutput(super.onStatusRemoved(target),[[],[`${target.name} is no loger paralized.`]]); }
   canApply(target:Character): boolean
-  { return super.canApply(target) && target.roundResistance.energyresistance<Math.random()*100; }
+  { return super.canApply(target) && target.calculated_resistance.energyresistance<Math.random()*100; }
   get tags(): tag[] { return super.tags.concat(['paralized'])}
 }
