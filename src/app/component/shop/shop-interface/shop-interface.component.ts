9e4e60da02fc3579fa53f60ca61f295bb6775e9d
zeyden <zeydenlaxharoth@gmail.com>
2021-12-17 18:46:14 -0600

    Arrays to fill lists of items calculated in method instead of using the method directrly from shop

--- src/app/component/shop/shop-interface/shop-interface.component.ts
+++ src/app/component/shop/shop-interface/shop-interface.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MasterService } from "src/app/service/master.service";
 import { ShopCurrentItemService } from 'src/app/service/shop-current-item.service';
 import { Character } from 'src/gameLogic/custom/Class/Character/Character';
+import { GameItem } from 'src/gameLogic/custom/Class/Items/Item';
 import { Shop } from 'src/gameLogic/custom/Class/Shop/Shop';
 
 @Component({
@@ -15,10 +16,15 @@ export class ShopInterfaceComponent implements OnInit {
   playerAmount: number = 1;
   shop:Shop;
   player:Character;
+  shopInventoryAfterSale:GameItem[]=[];
+  items2Shop:GameItem[]=[];
+  CharacterInvetoryAfterSale:GameItem[]=[];
+  items2Character:GameItem[]=[];
   constructor(private masterService:MasterService,private shopCurrentItemService:ShopCurrentItemService)
   {
     this.player = this.masterService.partyHandler.user;
     this.shop = this.masterService.descriptionHandler.currentDescription.descriptionData();
+    this.reloadShopArrays();
   }
 
   ngOnInit(): void {}
@@ -33,23 +39,34 @@ export class ShopInterfaceComponent implements OnInit {
   }
   shopSellItem(event:any)
   {
+    this.reloadShopArrays()
     this.shop.shopSellItem(event,this.shopAmount)
   }
   shopRemoveBuyItem(event:any)
   {
+    this.reloadShopArrays()
     this.shop.shopRemoveBuyItem(event,this.shopAmount)
   }
   shopBuyItem(event:any)
   {
+    this.reloadShopArrays()
     this.shop.shopBuyItem(event,this.playerAmount)
   }
   shopRemoveSellItem(event:any)
   {
+    this.reloadShopArrays()
     this.shop.shopRemoveSellItem(event,this.playerAmount)
   }
   unsetItem(event:Event)
   {
     event?.stopPropagation();
     this.shopCurrentItemService.currentItem = null;
+  }
+
+  private reloadShopArrays() {
+    this.shopInventoryAfterSale = this.shop.shopInventoryAfterSale;
+    this.items2Shop = this.shop.sale.items2Shop;
+    this.CharacterInvetoryAfterSale = this.shop.getCharacterInvetoryAfterSale(this.player);
+    this.items2Character = this.shop.sale.items2Character;
   }
 }
