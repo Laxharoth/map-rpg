a3ccb13488bf286a45093f5a671bfae05792e1b6
zeyden <zeydenlaxharoth@gmail.com>
2021-12-26 20:06:05 -0600

    Replaced hitpoints check for Character.is_defeated

--- src/gameLogic/custom/Class/Battle/Battle.functions.ts
+++ src/gameLogic/custom/Class/Battle/Battle.functions.ts
@@ -1,8 +1,7 @@
-import { DescriptionOptions } from 'src/gameLogic/custom/Class/Descriptions/Description';
 import { Character } from 'src/gameLogic/custom/Class/Character/Character';
 
 export function get_undefeated_target(group:Character[]):Character[]
-{ return group.filter(character => character.current_energy_stats.hitpoints>0); }
+{ return group.filter(character => !character.is_defeated()); }
 
 export function attack_order(characters:Character[]):Character[] {
   return characters.sort((character,other)=> character.calculated_stats.initiative > other.calculated_stats.initiative ? -1:1)
--- src/gameLogic/custom/Class/Character/NPC/EnemyFormations/EnemyFormation.ts
+++ src/gameLogic/custom/Class/Character/NPC/EnemyFormations/EnemyFormation.ts
@@ -30,7 +30,7 @@ export abstract class EnemyFormation
    * @memberof EnemyFormation
    */
   constructor( masterService:MasterService){this.masterService =masterService;}
-  get IsDefeated():boolean{ return this.enemies.every(character=>character.current_energy_stats.hitpoints<=0); }
+  get IsDefeated():boolean{ return this.enemies.every(character=>character.is_defeated()); }
   /**
    * Gets the private array of enemies.
    *
