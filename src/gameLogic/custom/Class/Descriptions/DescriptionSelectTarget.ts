f1c72f7202a4f40d5cac9f7cd3ece3eee8c691a0
zeyden <zeydenlaxharoth@gmail.com>
2021-12-26 20:20:25 -0600

    selectTarget moved to different file, added parameters -masterService:MasterService -targets:Character[] -playerAction:(target:Character[])=>void

---
+++ src/gameLogic/custom/Class/Descriptions/DescriptionSelectTarget.ts
@@ -0,0 +1,33 @@
+import { MasterService } from "src/app/service/master.service";
+import { MAXOPTIONSNUMBERPERPAGE } from "../../customTypes/constants";
+import { Character } from "../Character/Character";
+import { nextOption } from "./CommonOptions";
+import { Description, DescriptionOptions } from "./Description";
+
+/**
+ * Returns options to select target.
+ *
+ * @param {Character[]} targets
+ * @param {(target:Character[])=>ActionOutput} playerAction
+ * @return {*}  {Description}
+ */
+ export function selectTarget(masterService:MasterService,targets:Character[],playerAction:(target:Character[])=>void):Description
+ {
+   const targetsOptions:DescriptionOptions[] = [];
+   const returnOption = nextOption(masterService,'return');
+   for(const target of targets)
+   {
+     targetsOptions.push(new DescriptionOptions(target.name,()=>{ playerAction([target]) }))
+   }
+   if(targetsOptions.length <= MAXOPTIONSNUMBERPERPAGE)
+   {
+     while(targetsOptions.length < MAXOPTIONSNUMBERPERPAGE-1) targetsOptions.push(null);
+     targetsOptions.push(returnOption)
+   }
+   else
+   {
+     while(targetsOptions.length%MAXOPTIONSNUMBERPERPAGE-2 !==MAXOPTIONSNUMBERPERPAGE-3) targetsOptions.push(null);
+     targetsOptions.push(returnOption)
+   }
+   return new Description(()=>`${targets.map(target=>`${target.name}:${target.current_energy_stats.hitpoints}`).join('\n')}`,targetsOptions)
+ }
--- src/gameLogic/custom/Class/Descriptions/DescriptionUseItem.ts
+++ src/gameLogic/custom/Class/Descriptions/DescriptionUseItem.ts
@@ -45,33 +45,4 @@ export function selectItem(masterService:MasterService):Description
     options.push(returnOption)
   }
   return new Description(()=>`${items.map(item=>item.name).join('\n')}`,options)
-  ///////////////////////////////////////////////////////
-  //SELECT TARGET
-  ///////////////////////////////////////////////////////
-  /**
-   * Returns options to select target.
-   *
-   * @param {Character[]} targets
-   * @param {(target:Character[])=>ActionOutput} playerAction
-   * @return {*}  {Description}
-   */
-   function selectTarget(targets:Character[],playerAction:(target:Character[])=>void):Description
-   {
-     const targetsOptions:DescriptionOptions[] = [];
-     for(const target of targets)
-     {
-       targetsOptions.push(new DescriptionOptions(target.name,()=>{ playerAction([target]) }))
-     }
-     if(targetsOptions.length <= MAXOPTIONSNUMBERPERPAGE)
-     {
-       while(targetsOptions.length < MAXOPTIONSNUMBERPERPAGE-1) targetsOptions.push(null);
-       targetsOptions.push(returnOption)
-     }
-     else
-     {
-       while(targetsOptions.length%MAXOPTIONSNUMBERPERPAGE-2 !==MAXOPTIONSNUMBERPERPAGE-3) targetsOptions.push(null);
-       targetsOptions.push(returnOption)
-     }
-     return new Description(()=>`${targets.map(target=>`${target.name}:${target.current_energy_stats.hitpoints}`).join('\n')}`,targetsOptions)
-   }
 }
