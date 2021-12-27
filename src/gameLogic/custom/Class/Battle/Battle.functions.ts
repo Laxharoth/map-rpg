c82bfced51a66d303a9651cbf59e346d8d547ff6
zeyden <zeydenlaxharoth@gmail.com>
2021-12-26 20:09:03 -0600

    shortened attack_order

--- src/gameLogic/custom/Class/Battle/Battle.functions.ts
+++ src/gameLogic/custom/Class/Battle/Battle.functions.ts
@@ -4,5 +4,5 @@ export function get_undefeated_target(group:Character[]):Character[]
 { return group.filter(character => !character.is_defeated()); }
 
 export function attack_order(characters:Character[]):Character[] {
-  return characters.sort((character,other)=> character.calculated_stats.initiative > other.calculated_stats.initiative ? -1:1)
+  return characters.sort((character,other)=> other.calculated_stats.initiative-character.calculated_stats.initiative)
 }
