fcdb1a20472d5ba83920cb0e3b52b4eed63de80f
zeyden <zeydenlaxharoth@gmail.com>
2021-12-28 18:42:21 -0600

    Added PoisonRush status

--- src/gameLogic/custom/Class/Character/Reaction/ReactionPoisonRush.ts
+++ src/gameLogic/custom/Class/Character/Reaction/ReactionPoisonRush.ts
@@ -1,5 +1,6 @@
 import { Reaction } from 'src/gameLogic/custom/Class/Character/Reaction/Reaction';
 import { tag } from "src/gameLogic/custom/customTypes/tags";
+import { PoisonRush } from '../../Status/StatusTemporal/PoisonRush';
 import { Character } from "../Character";
 import { ActionOutput } from "../Character.type";
 
@@ -7,7 +8,6 @@ export class PoisonRushReaction extends Reaction
 {
   protected whatTriggers: tag[][] = [['status ended' , 'poison']];
   protected action(react_character: Character,source:Character,target: Character[]): ActionOutput {
-    react_character.calculated_stats.physical_attack*=4;
-      return [[],['Overcoming poison grants extra attack']]
+    return react_character.addStatus(new PoisonRush(this.masterService))
   }
 }
