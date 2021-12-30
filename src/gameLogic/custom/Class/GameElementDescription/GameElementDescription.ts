e3d79ad03dca49a9761fb0128ae6914c2acf411e
zeyden <zeydenlaxharoth@gmail.com>
2021-12-29 21:16:39 -0600

    Added interface descriptable
    -Added interface descriptable
    -Added interfaces GameElementDescriptionSection, game_element_description_section_item
    -Added section names

---
+++ src/gameLogic/custom/Class/GameElementDescription/GameElementDescription.ts
@@ -0,0 +1,25 @@
+import { primitive } from "src/gameLogic/core/types";
+
+export interface GameElementDescriptionSection
+{
+  name: section_names;
+  section_items:game_element_description_section_item[]
+}
+export interface game_element_description_section_item
+{
+  name: string;
+  value:primitive;
+}
+export interface descriptable
+{
+  get description():GameElementDescriptionSection[];
+}
+enum section_names_enum {
+  tags="tags",
+  cooldown="cooldown",
+  description="description",
+  stats="stats",
+  resistance="resistance",
+  damage="damage",
+}
+export type section_names = `${section_names_enum}`
