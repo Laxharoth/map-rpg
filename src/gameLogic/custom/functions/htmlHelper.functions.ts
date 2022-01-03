e11f633af95b2d6bbe42d5e5c7759c59d1b51571
zeyden <zeydenlaxharoth@gmail.com>
2022-01-02 18:32:18 -0600

    Added compare_array function

--- src/gameLogic/custom/functions/htmlHelper.functions.ts
+++ src/gameLogic/custom/functions/htmlHelper.functions.ts
@@ -120,3 +120,11 @@ export const set_theme = (()=>{
     localStorage.setItem('theme',themes[theme_name]);
   }
 })();
+
+export function compare_array<T>(array1:T[],array2:T[]): boolean
+{
+  if(array1.length != array2.length)return false;
+  for(let i = 0; i < array1.length; i++)
+    if(array1[i] !== array2[i])return false;
+  return true;
+}
