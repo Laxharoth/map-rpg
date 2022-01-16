9c4ea374d6ba91102614618740d8ce0eafe8480b
zeyden <zeydenlaxharoth@gmail.com>
2022-01-15 19:38:48 -0600

    Updated Module path

--- src/gameLogic/custom/functions/load_files.ts
+++ src/gameLogic/custom/functions/load_files.ts
@@ -3,6 +3,6 @@ import { register_quest } from "../Class/Quest/QuestFactory";
 export async function load_files({})
 {
   //@ts-ignore
-  await import("../../../assets/Quest/DefeatEnemyQuest.js").then(module=>register_quest(module))
+  await import("../../../assets/Load/Quest/DefeatEnemyQuest.js").then(module=>register_quest(module))
 
 }
