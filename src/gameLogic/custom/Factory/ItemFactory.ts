85a068605248951685701eeb0d0ebc8a33364148
zeyden <zeydenlaxharoth@gmail.com>
2022-01-16 21:34:47 -0600

    Added function to register Item modules

--- src/gameLogic/custom/Factory/ItemFactory.ts
+++ src/gameLogic/custom/Factory/ItemFactory.ts
@@ -1,20 +1,23 @@
-import { PoisonPill } from './../Class/Items/PoisonPill';
+import { MeleeWeapon } from 'src/gameLogic/custom/Class/Equipment/Weapon/Melee/MeleeWeapon';
 import { ItemStoreable } from './../Class/Items/Item';
 import { MasterService } from 'src/app/service/master.service';
-import { ArmorNoArmor } from 'src/gameLogic/custom/Class/Equipment/Armor/Armor';
+import { Armor, ArmorNoArmor } from 'src/gameLogic/custom/Class/Equipment/Armor/Armor';
 import { ArmorTest } from 'src/gameLogic/custom/Class/Equipment/Armor/ArmorTest';
-import { ShieldNoShield } from 'src/gameLogic/custom/Class/Equipment/Shield/Shield';
+import { Shield, ShieldNoShield } from 'src/gameLogic/custom/Class/Equipment/Shield/Shield';
 import { ShieldTest } from 'src/gameLogic/custom/Class/Equipment/Shield/ShieldTest';
 import { MeleeTest } from 'src/gameLogic/custom/Class/Equipment/Weapon/Melee/MeleeTest';
 import { MeleeUnharmed } from 'src/gameLogic/custom/Class/Equipment/Weapon/Melee/MeleeWeapon';
 import { RangedTest } from 'src/gameLogic/custom/Class/Equipment/Weapon/Ranged/RangedTest';
-import { RangedUnharmed } from 'src/gameLogic/custom/Class/Equipment/Weapon/Ranged/RangedWeapon';
+import { RangedUnharmed, RangedWeapon } from 'src/gameLogic/custom/Class/Equipment/Weapon/Ranged/RangedWeapon';
 import { GameItem } from 'src/gameLogic/custom/Class/Items/Item';
 import { armornameEnum, itemsEnum, meleenameEnum, rangednameEnum, shieldnameEnum } from 'src/gameLogic/custom/Class/Items/Item.type';
 import { ItemTest } from 'src/gameLogic/custom/Class/Items/ItemTest';
 import { ShieldGuard } from '../Class/Equipment/Shield/ShieldGuard';
+import { FactoryFunction } from 'src/gameLogic/configurable/Factory/FactoryMap';
+import { status_factory } from './StatusFactory';
+import { add_module, Factory } from 'src/gameLogic/core/Factory/Factory';
 
-
+export type item_factory_function = (masterService: MasterService, options: ItemStoreable)=>GameItem
 /**
  * Creates an Item with the given itemname
  *
@@ -31,8 +34,9 @@ export function ItemFactory(masterService:MasterService,options:ItemStoreable):G
   return item;
 }
 
-/** @type {[key: string]:Item.constructor} */
-const ItemSwitcher:{[key in meleenameEnum| rangednameEnum| shieldnameEnum| armornameEnum| itemsEnum]:any} = {
+interface ItemConstructor { new (masterService:MasterService):GameItem }
+//@ts-ignore
+const ItemSwitcher:{[key in meleenameEnum| rangednameEnum| shieldnameEnum| armornameEnum| itemsEnum]:ItemConstructor} = {
   'hand':MeleeUnharmed,
   'Melee test':MeleeTest,
   'a rock':RangedUnharmed,
@@ -43,5 +47,35 @@ const ItemSwitcher:{[key in meleenameEnum| rangednameEnum| shieldnameEnum| armor
   'Armor Test':ArmorTest,
   'item-test':ItemTest,
   'Guard Shield':ShieldGuard,
-  'Poison Pill':PoisonPill,
+  // 'Poison Pill':PoisonPill,
+}
+
+export type register_item_function = (
+  ItemSwitcher:{[key: string]:ItemConstructor},
+  ItemConstructors:item_constructors,
+  Factory:FactoryFunction
+  )=>void;
+type item_module = { register:register_item_function }
+type item_constructors =  {
+  GameItem: typeof GameItem;
+  MeleeWeapon: typeof MeleeWeapon;
+  RangedWeapon: typeof RangedWeapon;
+  Shield: typeof Shield;
+  Armor: typeof Armor;
+}
+export const  ItemConstructors = {
+  GameItem,
+  MeleeWeapon,
+  RangedWeapon,
+  Shield,
+  Armor,
+}
+export function register_item(item_module:item_module):void
+{
+  //@ts-ignore
+  const  { register, module_name, module_dependency } = item_module;
+  add_module({
+    register_function(){register(ItemSwitcher,ItemConstructors,Factory)},
+    module_name, module_dependency
+  })
 }
