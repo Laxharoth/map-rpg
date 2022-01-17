cc00068e4e802217d56fd67be03130d1c6e9d543
zeyden <zeydenlaxharoth@gmail.com>
2022-01-16 21:36:16 -0600

    Added functions to add external modules

--- src/gameLogic/core/Factory/Factory.ts
+++ src/gameLogic/core/Factory/Factory.ts
@@ -1,3 +1,4 @@
+import { removeItem } from 'src/gameLogic/custom/functions/htmlHelper.functions';
 import { MasterService } from "src/app/service/master.service";
 import { factoryMap, factoryname } from "src/gameLogic/configurable/Factory/FactoryMap";
 import { gamesavenames } from "src/gameLogic/configurable/subservice/game-saver.type";
@@ -9,4 +10,27 @@ export type StoreableType = {Factory:factoryname,type: string,dependency_gamesav
 export interface storeable {
   toJson():StoreableType;
   fromJson(options:StoreableType):void;
+}
+
+const registered_modules_set = new Set<string>(["Poison"])
+type register_object = {register_function:()=>void,module_name:string,module_dependency:string[]}
+const waiting_modules:register_object[] = []
+export function add_module(module:register_object):void
+{
+  let previous_waiting_modules_length = waiting_modules.length
+  waiting_modules.push(module)
+  while(previous_waiting_modules_length!==waiting_modules.length)
+  {
+    previous_waiting_modules_length = waiting_modules.length
+    const remove_modules:register_object[] = []
+    for(const module of waiting_modules)if(register_module(module))remove_modules.push(module)
+    for(const module of remove_modules )removeItem(waiting_modules, module)
+  }
+}
+export function register_module({register_function,module_name,module_dependency}:register_object):boolean
+{
+  if(module_dependency.some(module=>!registered_modules_set.has(module)))return false;
+  register_function();
+  registered_modules_set.add(module_name)
+  return true;
 }
