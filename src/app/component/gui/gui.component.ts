9aafa5010aef2a9d3b562b8058f3263c811b415f
Laxharoth <zeydenlaxharoth@gmail.com>
2022-03-10 13:04:22 -0600

    extracted custom code from gui.component

--- src/app/component/gui/gui.component.ts
+++ src/app/component/gui/gui.component.ts
@@ -1,24 +1,13 @@
-import { InfoPageToggler } from 'src/gameLogic/custom/subservice/info-page-toggler';
 import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { MasterService } from "src/app/service/master.service";
 import { FlagHandlerService } from 'src/gameLogic/core/subservice/flag-handler';
 import { GameSaver } from 'src/gameLogic/core/subservice/game-saver';
-import { MainCharacter } from 'src/gameLogic/custom/Class/Character/MainCharacter/MainCharacter';
-import { SceneHandlerService } from 'src/gameLogic/custom/subservice/scene-handler';
 import { GameStateService } from 'src/gameLogic/core/subservice/game-state';
 import { game_state } from 'src/gameLogic/configurable/subservice/game-state.type';
-import { LockMapService } from 'src/gameLogic/custom/subservice/lock-map';
-import { MapHandlerService } from 'src/gameLogic/custom/subservice/map-handler';
-import { PartyService } from 'src/gameLogic/custom/subservice/party';
-import { TimeHandler } from 'src/gameLogic/custom/subservice/time-handler';
-import { FactWeb } from 'src/gameLogic/custom/subservice/fact-web';
-import { UniqueCharacterHandler } from 'src/gameLogic/custom/subservice/unique-character-handler';
 import { set_theme } from 'src/gameLogic/custom/functions/htmlHelper.functions';
 import { load_files } from 'src/gameLogic/custom/functions/load_files';
-import { QuestHolder } from 'src/gameLogic/custom/subservice/quest-holder';
-import { ItemFactory } from 'src/gameLogic/custom/Factory/ItemFactory';
-import { Factory } from 'src/gameLogic/core/Factory/Factory';
+import { entryPoint } from 'src/gameLogic/entryPoint';
 
 @Component({
   selector   : 'app-gui',
@@ -27,7 +16,6 @@ import { Factory } from 'src/gameLogic/core/Factory/Factory';
 })
 export class GuiComponent implements OnInit {
   currentGameState:game_state;
-
   private gameStateSubscription : Subscription;
 
   constructor(private masterService:MasterService) {
@@ -36,13 +24,7 @@ export class GuiComponent implements OnInit {
     this.register_master_service_subservice();
     this.InitializeSubscriptions();
     load_files({}).then(() => {
-      //debug to get savedata
-      this.masterService.gameSaver.load("save1");
-
-      this.FirstTimeUserInitialize();
-
-      this.masterService.mapHandler.loadRoom(this.masterService.flagsHandler.getFlag("currentroom"));
-      this.masterService.timeHandler.addTime(0);
+      entryPoint(masterService);
     })
   }
 
@@ -58,47 +40,10 @@ export class GuiComponent implements OnInit {
   }
   private register_master_service_subservice() {
     const gameSaver  = new GameSaver(this.masterService);
-    const lockmap      = new LockMapService();
-    const gameStateHandler   = new GameStateService();
-    const mapHandler   = new MapHandlerService(this.masterService,gameStateHandler,lockmap);
-    const sceneHandler = new SceneHandlerService(lockmap, gameStateHandler);
     const flagsHandler = new FlagHandlerService(gameSaver);
-    const timeHandler = new TimeHandler(gameSaver)
-    const unique_characters_handler = new UniqueCharacterHandler(gameSaver);
-    const partyHandler = new PartyService(gameSaver,unique_characters_handler);
-    const data_web = new FactWeb(timeHandler, gameSaver,unique_characters_handler);
-    const info_page_toggler = new InfoPageToggler(sceneHandler);
-    const quest_holder = new QuestHolder(gameSaver,this.masterService);
-    this.masterService.register("lockmap", lockmap);
-    this.masterService.register("flagsHandler", flagsHandler);
-    this.masterService.register("partyHandler", partyHandler);
-    this.masterService.register("gameStateHandler", gameStateHandler);
-    this.masterService.register("sceneHandler", sceneHandler);
-    this.masterService.register("mapHandler", mapHandler);
+    const gameStateHandler   = new GameStateService();
     this.masterService.register("gameSaver", gameSaver);
-    this.masterService.register("timeHandler",timeHandler);
-    this.masterService.register("FactWeb",data_web);
-    this.masterService.register("UniqueCharacterHandler",unique_characters_handler);
-    this.masterService.register("InfoPageToggler",info_page_toggler);
-    this.masterService.register("QuestHolder",quest_holder);
-  }
-  private FirstTimeUserInitialize() {
-    if(this.masterService.gameSaver?.MainCharacter?.[0]) {
-      this.masterService.partyHandler.user = this.masterService.gameSaver.MainCharacter[0];
-    }
-    if (!this.masterService.partyHandler.user) {
-      const user = new MainCharacter(this.masterService, 'player',"TestMainCharacterBattleClass");
-      const meleeTest1 = ItemFactory(this.masterService,{ Factory:"Item",type:"MeleeTest"})
-      const rangedTest1 = ItemFactory(this.masterService,{ Factory:"Item",type:"RangedTest"})
-      const shieldTest1 = ItemFactory(this.masterService,{ Factory:"Item",type:"ShieldTest"})
-      const armorTest1 = ItemFactory(this.masterService,{ Factory:"Item",type:"ArmorTest"})
-      user.inventory.addItem(meleeTest1); user.inventory.addItem(rangedTest1); user.inventory.addItem(shieldTest1); user.inventory.addItem(armorTest1);
-      this.masterService.partyHandler.user = user;
-      this.masterService.partyHandler.setPartyMember(Factory(this.masterService,{
-        Factory:"Character",
-        type:"test character",
-        name:"ally 1",
-      }),0);
-    }
+    this.masterService.register("flagsHandler", flagsHandler);
+    this.masterService.register("gameStateHandler", gameStateHandler);
   }
 }
---
+++ src/gameLogic/entryPoint.ts
@@ -0,0 +1,65 @@
+import { MasterService } from "src/app/service/master.service";
+import { Factory } from "./core/Factory/Factory";
+import { MainCharacter } from "./custom/Class/Character/MainCharacter/MainCharacter";
+import { ItemFactory } from "./custom/Factory/ItemFactory";
+import { FactWeb } from "./custom/subservice/fact-web";
+import { InfoPageToggler } from "./custom/subservice/info-page-toggler";
+import { LockMapService } from "./custom/subservice/lock-map";
+import { MapHandlerService } from "./custom/subservice/map-handler";
+import { PartyService } from "./custom/subservice/party";
+import { QuestHolder } from "./custom/subservice/quest-holder";
+import { SceneHandlerService } from "./custom/subservice/scene-handler";
+import { TimeHandler } from "./custom/subservice/time-handler";
+import { UniqueCharacterHandler } from "./custom/subservice/unique-character-handler";
+
+export function entryPoint(masterService:MasterService){
+  // Initialize custom services
+  const { gameSaver, gameStateHandler } = masterService;
+  const lockmap      = new LockMapService();
+  const mapHandler   = new MapHandlerService(masterService,gameStateHandler,lockmap);
+  const sceneHandler = new SceneHandlerService(lockmap, gameStateHandler);
+  const timeHandler = new TimeHandler(gameSaver)
+  const unique_characters_handler = new UniqueCharacterHandler(gameSaver);
+  const partyHandler = new PartyService(gameSaver,unique_characters_handler);
+  const data_web = new FactWeb(timeHandler, gameSaver,unique_characters_handler);
+  const info_page_toggler = new InfoPageToggler(sceneHandler);
+  const quest_holder = new QuestHolder(gameSaver,masterService);
+  masterService.register("lockmap", lockmap);
+  masterService.register("partyHandler", partyHandler);
+  masterService.register("sceneHandler", sceneHandler);
+  masterService.register("mapHandler", mapHandler);
+  masterService.register("timeHandler",timeHandler);
+  masterService.register("FactWeb",data_web);
+  masterService.register("UniqueCharacterHandler",unique_characters_handler);
+  masterService.register("InfoPageToggler",info_page_toggler);
+  masterService.register("QuestHolder",quest_holder);
+  FirstTimeUserInitialize(masterService);
+  //debug to get savedata
+  masterService.gameSaver.load("save1");
+  masterService.mapHandler.loadRoom(masterService.flagsHandler.getFlag("currentroom"));
+  masterService.timeHandler.addTime(0);
+}
+
+function FirstTimeUserInitialize(masterService:MasterService) {
+  if(masterService.gameSaver?.MainCharacter?.[0]) {
+    masterService.partyHandler.user = masterService.gameSaver.MainCharacter[0];
+  }
+  if (!masterService.partyHandler.user) {
+    const user = new MainCharacter(masterService, 'player',"TestMainCharacterBattleClass");
+    const meleeTest1 = ItemFactory(masterService,{ Factory:"Item",type:"MeleeTest"})
+    const rangedTest1 = ItemFactory(masterService,{ Factory:"Item",type:"RangedTest"})
+    const shieldTest1 = ItemFactory(masterService,{ Factory:"Item",type:"ShieldTest"})
+    const armorTest1 = ItemFactory(masterService,{ Factory:"Item",type:"ArmorTest"})
+    user.inventory.addItem(meleeTest1); user.inventory.addItem(rangedTest1); user.inventory.addItem(shieldTest1); user.inventory.addItem(armorTest1);
+    masterService.partyHandler.user = user;
+    masterService.partyHandler.setPartyMember(Factory(masterService,{
+      Factory:"Character",
+      type:"test character",
+      name:"ally 1",
+    }),0);
+  }
+}
+
+export function newGame(masterService:MasterService){
+
+}
