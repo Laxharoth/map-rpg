eeaf8f63e44cc76f61359cfda2115e59b6ee021f
Laxharoth <zeydenlaxharoth@gmail.com>
2022-03-17 15:31:14 -0600

    fixed type of EnemyFormationConstructor

--- src/gameLogic/custom/Factory/EnemyFormationFactory.ts
+++ src/gameLogic/custom/Factory/EnemyFormationFactory.ts
@@ -3,13 +3,12 @@ import { FactoryFunction } from "src/gameLogic/configurable/Factory/FactoryMap"
 import { EnemyFormation, EnemyFormationOptions } from "../Class/Character/NPC/EnemyFormations/EnemyFormation"
 
 export const EnemyFormationFactory:FactoryFunction<EnemyFormation,EnemyFormationOptions> = (masterService,options)=>{
-  return new enemy_formation_switcher[options.type](masterService,options)
+  const formation:EnemyFormation = new enemy_formation_switcher[options.type](masterService)
+  formation.fromJson(options)
+  return formation;
 }
 
 interface EnemyFormationConstructor{
-  new:(masterService:MasterService)=>EnemyFormation
+  new(masterService:MasterService):EnemyFormation
 }
-/** @type { [key: string]:EnemyFormationConstructor } */
-export const enemy_formation_switcher = {
-
-}
+export const enemy_formation_switcher:{ [key: string]:EnemyFormationConstructor }  = {}
