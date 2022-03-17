815faaff6948a7c9ac889a706a6f3c814a72f8b6
Laxharoth <zeydenlaxharoth@gmail.com>
2022-03-16 18:55:46 -0600

    Added  complete method and status attribute

--- src/gameLogic/custom/Class/Quest/Quest.ts
+++ src/gameLogic/custom/Class/Quest/Quest.ts
@@ -1,28 +1,33 @@
 import { descriptable, GameElementDescriptionSection } from 'src/gameLogic/custom/Class/GameElementDescription/GameElementDescription';
 import { storeable } from "src/gameLogic/core/Factory/Factory";
 import { hashable } from '../../ClassHelper/ObjectSet';
+import { questnames, questStatus } from './Quest.type';
 
 export abstract class Quest implements storeable, descriptable,hashable
 {
-  abstract type: string;
+  abstract type: questnames;
   abstract name: string;
   abstract description_text: string;
   abstract toJson(): QuestOptions;
   abstract fromJson(options: QuestOptions): void;
+  status: questStatus="in progress";
+
   get add_description():GameElementDescriptionSection[]{return []};
-  get description(): GameElementDescriptionSection[]
-  {
+  get description(): GameElementDescriptionSection[]{
     return [
       {name:"name",section_items:[{name:'name',value:this.name}]},
       {name:"description",section_items:[{name:'description',value:this.description_text}]},
       ...this.add_description
     ]
   }
+  /** sets the status based on the quest conditions.  */
+  abstract complete():void;
   //@ts-ignore
   hash(): string { return this.constructor }
 }
 export type QuestOptions = {
   Factory: 'Quest',
-  type: "DefeatEnemyQuest",
+  type: questnames,
+  status: questStatus,
   [key:string]:any
 }
---
+++ src/gameLogic/custom/Class/Quest/Quest.type.ts
@@ -0,0 +1,6 @@
+enum QuestNames{
+  "DefeatEnemyQuest"="DefeatEnemyQuest",
+  "FoolDragonSeller"="FoolDragonSeller",
+}
+export type questStatus = "in progress"|"complete"|"failed";
+export type questnames = `${QuestNames}`
