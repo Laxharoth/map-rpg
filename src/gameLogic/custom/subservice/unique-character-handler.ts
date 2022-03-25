ec7f9398179dd519e6a5eae60ea5baeb90851c49
Laxharoth <zeydenlaxharoth@gmail.com>
2022-03-24 20:59:06 -0600

    Update unique-character-handler.ts
    replaced removed uuid for type
    get_character checks if ht character exists if not tries to create the character.

--- src/gameLogic/custom/subservice/unique-character-handler.ts
+++ src/gameLogic/custom/subservice/unique-character-handler.ts
@@ -1,18 +1,29 @@
+import { CharacterFactory } from 'src/gameLogic/custom/Factory/CharacterFactory';
 import { UniqueCharacter } from "src/gameLogic/custom/Class/Character/UniqueCharacter";
-import { GameSaver } from "src/gameLogic/core/subservice/game-saver";
+
+import { characterType } from "../Factory/CharacterFactory";
+import { MasterService } from "src/app/service/master.service";
+import { Factory } from "src/gameLogic/core/Factory/Factory";
 
 export class UniqueCharacterHandler
 {
   private _characters:{[key: string]:UniqueCharacter}={};
-
-  constructor(game_saver: GameSaver){
+  private readonly masterService: MasterService;
+  constructor(masterService:MasterService){
+    this.masterService = masterService;
+    const {gameSaver:game_saver} = masterService;
     game_saver.on_change_persistent_instance().subscribe(([instance_name,action,character])=>{
       const unique_character = character as UniqueCharacter;
       if(!(instance_name==="MainCharacter" || instance_name==="PersistentCharacter"))return;
-      if(action==="unregister"){delete this._characters[unique_character.uuid];return;}
-      this._characters[unique_character.uuid] = unique_character
+      if(action==="unregister"){delete this._characters[unique_character.type];return;}
+      this._characters[unique_character.type] = unique_character
     })
   }
-  get_character(uuid: string):UniqueCharacter{ return this._characters[uuid]; }
+  get_character(type: string):UniqueCharacter{
+    let character:UniqueCharacter|undefined = this._characters[type];
+    if(!character)
+      character = (Factory as typeof CharacterFactory)(this.masterService,{ Factory:"Character",type:type as characterType  }) as UniqueCharacter
+    return character;
+  }
   get unique_characters():UniqueCharacter[]{ return Object.values(this._characters); }
 }
