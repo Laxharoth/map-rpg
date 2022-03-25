4c0c78aac9af5809f4bf4c00e23456d00a9653e7
Laxharoth <zeydenlaxharoth@gmail.com>
2022-03-24 20:12:07 -0600

    Update DamageSource.ts
    calculateDamage fixed to check undefined values
    added fixed damage damage types
    added attack function

--- src/gameLogic/custom/Class/Battle/DamageSource.ts
+++ src/gameLogic/custom/Class/Battle/DamageSource.ts
@@ -1,24 +1,41 @@
+import { pushBattleActionOutput } from "../../functions/htmlHelper.functions";
 import { Character } from "../Character/Character";
+import { ActionOutput } from "../Character/Character.type";
 
-export interface DamageSource
-{
-  damageTypes:FullDamageTypes
+export interface DamageSource{
+  damageTypes:DamageTypes;
   damagestat(user   : Character):number;
   defencestat(target: Character):number;
+  didAttackMiss(source:Character, target:Character):boolean;
+  attackLanded(damage:number,user:Character,target:Character):ActionOutput;
+  attackMissed(user:Character,target:Character):ActionOutput;
+  onAttackMissed?:(user:Character,target:Character)=>ActionOutput;
+  onAttackLanded?:(user:Character,target:Character)=>ActionOutput;
 }
-
-export function calculateDamage(damage_source: DamageSource,user:Character,target:Character):number
-{
+export function calculateDamage(damage_source: DamageSource,user:Character,target:Character):number{
   let finalDamage:number = 0;
   const damageRelation = damage_source.damagestat(user) / damage_source.defencestat(target);
-  finalDamage += (damageRelation * damage_source.damageTypes.slashdamage ||0 / (100 - target.calculated_resistance.slashresistance));
-  finalDamage += (damageRelation * damage_source.damageTypes.bluntdamage ||0 / (100 - target.calculated_resistance.bluntresistance));
-  finalDamage += (damageRelation * damage_source.damageTypes.piercedamage||0 / (100 - target.calculated_resistance.pierceresistance));
-  finalDamage += (damageRelation * damage_source.damageTypes.poisondamage||0 / (100 - target.calculated_resistance.poisonresistance));
-  finalDamage += (damageRelation * damage_source.damageTypes.heatdamage  ||0 / (100 - target.calculated_resistance.heatresistance));
-  finalDamage += (damageRelation * damage_source.damageTypes.energydamage||0 / (100 - target.calculated_resistance.energyresistance));
-  finalDamage += (damageRelation * damage_source.damageTypes.frostdamage ||0 / (100 - target.calculated_resistance.frostresistance));
+  finalDamage += (damageRelation * ( damage_source.damageTypes.slashdamage || 0 ) / ( 100 - target.calculated_resistance.slashresistance ) );
+  finalDamage += (damageRelation * ( damage_source.damageTypes.bluntdamage || 0 ) / ( 100 - target.calculated_resistance.bluntresistance ) );
+  finalDamage += (damageRelation * ( damage_source.damageTypes.piercedamage|| 0 ) / ( 100 - target.calculated_resistance.pierceresistance ) );
+  finalDamage += (damageRelation * ( damage_source.damageTypes.poisondamage|| 0 ) / ( 100 - target.calculated_resistance.poisonresistance ) );
+  finalDamage += (damageRelation * ( damage_source.damageTypes.heatdamage  || 0 ) / ( 100 - target.calculated_resistance.heatresistance ) );
+  finalDamage += (damageRelation * ( damage_source.damageTypes.energydamage|| 0 ) / ( 100 - target.calculated_resistance.energyresistance ) );
+  finalDamage += (damageRelation * ( damage_source.damageTypes.frostdamage || 0 ) / ( 100 - target.calculated_resistance.frostresistance ) );
   return Math.round(finalDamage)||0;
+}
+export function attack(damageSource:DamageSource,source:Character,target:Character){
+  if(damageSource.didAttackMiss(source,target)){
+    return pushBattleActionOutput(
+      damageSource.onAttackMissed?.(source,target)||[[],[]],
+      damageSource.attackMissed(source,target)
+    )
+  }
+  const damage = calculateDamage(damageSource,source,target);
+  return pushBattleActionOutput(
+    damageSource.onAttackLanded?.(source,target)||[[],[]],
+    damageSource.attackLanded(damage,source,target)
+  )
 }
 export interface DamageTypes {
   heatdamage ? : number;
@@ -28,6 +45,7 @@ export interface DamageTypes {
   bluntdamage ? : number;
   piercedamage ? : number;
   poisondamage ? : number;
+  fixeddamage ? : number;
 }
 export interface FullDamageTypes {
   heatdamage : number;
@@ -37,4 +55,5 @@ export interface FullDamageTypes {
   bluntdamage : number;
   piercedamage : number;
   poisondamage : number;
+  fixeddamage : number;
 }
