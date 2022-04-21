64b58b67393e0c3d20ced584b013d3c509c5de06
Laxharoth <zeydenlaxharoth@gmail.com>
2022-04-21 11:43:48 -0500

    moved ModuleFunctions to FactoryMap
    ModuleFunctions were in core directory which should not change, but these are ment to be added as they are needed

--- src/gameLogic/configurable/Factory/FactoryMap.ts
+++ src/gameLogic/configurable/Factory/FactoryMap.ts
@@ -1,22 +1,27 @@
-import { BattleFactory } from './../../custom/Factory/BattleFactory';
 import { MasterService } from "src/app/service/master.service";
+import { Factory, StoreableType } from "src/gameLogic/core/Factory/Factory";
 import { MasterFlagsSetter } from "src/gameLogic/core/subservice/flag-handler";
-import { QuestFactory } from "src/gameLogic/custom/Factory/QuestFactory";
+import { escapeBattle } from 'src/gameLogic/custom/Class/Battle/Battle.functions';
+import { enterRoomOption, nextOption } from 'src/gameLogic/custom/Class/Scene/CommonOptions';
 import { ShopFactory } from "src/gameLogic/custom/Class/Shop/DynamicShop";
 import { UpgradeFactory } from "src/gameLogic/custom/Class/Upgrade/UpgradeFactory";
+import { CharacterBattleClassFactory } from "src/gameLogic/custom/Factory/CharacterBattleClassFactory";
 import { CharacterFactory } from "src/gameLogic/custom/Factory/CharacterFactory";
+import { EnemyFormationFactory } from "src/gameLogic/custom/Factory/EnemyFormationFactory";
 import { ItemFactory } from "src/gameLogic/custom/Factory/ItemFactory";
 import { PerkFactory } from "src/gameLogic/custom/Factory/PerkFactory";
+import { QuestFactory } from "src/gameLogic/custom/Factory/QuestFactory";
+import { ReactionFactory } from "src/gameLogic/custom/Factory/ReactionFactory";
+import { SpecialAttackFactory } from "src/gameLogic/custom/Factory/SpecialAttackFactory";
 import { StatusFactory } from "src/gameLogic/custom/Factory/StatusFactory";
+import { pushBattleActionOutput, randomBetween, randomCheck } from 'src/gameLogic/custom/functions/htmlHelper.functions';
 import { SetDataweb } from "src/gameLogic/custom/subservice/fact-web";
+import { enterRoom } from 'src/gameLogic/custom/subservice/map-handler';
 import { SetCurrentParty } from "src/gameLogic/custom/subservice/party";
 import { loadQuest } from "src/gameLogic/custom/subservice/quest-holder";
 import { SetTimeHandler } from "src/gameLogic/custom/subservice/time-handler";
-import { StoreableType } from "src/gameLogic/core/Factory/Factory";
-import { SpecialAttackFactory } from "src/gameLogic/custom/Factory/SpecialAttackFactory";
-import { ReactionFactory } from "src/gameLogic/custom/Factory/ReactionFactory";
-import { CharacterBattleClassFactory } from "src/gameLogic/custom/Factory/CharacterBattleClassFactory";
-import { EnemyFormationFactory } from "src/gameLogic/custom/Factory/EnemyFormationFactory";
+import { BattleFactory } from './../../custom/Factory/BattleFactory';
+
 
 export enum FactoryName{
   'Item'='Item',
@@ -39,7 +44,6 @@ export enum FactoryName{
 }
 export type factoryname= `${FactoryName}`;
 export type FactoryFunction<T=any,U=StoreableType> = (masterService:MasterService,options:U)=>T;
-
 export const factoryMap:{[key in FactoryName]:FactoryFunction} = {
   // @ts-ignore
   Item: ItemFactory,
@@ -76,3 +80,22 @@ export const factoryMap:{[key in FactoryName]:FactoryFunction} = {
   // @ts-ignore
   Battle:BattleFactory,
 }
+
+const factoryOptions = {
+  nextOption,
+  enterRoomOption,
+};
+export interface ModuleFunctions{
+  randomCheck:typeof randomCheck;
+  randomBetween:typeof randomBetween;
+  pushBattleActionOutput:typeof pushBattleActionOutput;
+  options:typeof factoryOptions;
+  escapeBattle:typeof escapeBattle;
+  enterRoom:typeof enterRoom;
+}
+(Factory as unknown as ModuleFunctions).randomCheck=randomCheck;
+(Factory as unknown as ModuleFunctions).randomBetween=randomBetween;
+(Factory as unknown as ModuleFunctions).pushBattleActionOutput=pushBattleActionOutput;
+(Factory as unknown as ModuleFunctions).escapeBattle=escapeBattle;
+(Factory as unknown as ModuleFunctions).enterRoom=enterRoom;
+(Factory as unknown as ModuleFunctions).options = factoryOptions;
--- src/gameLogic/core/Factory/Factory.ts
+++ src/gameLogic/core/Factory/Factory.ts
@@ -1,39 +1,17 @@
-import { randomCheck, pushBattleActionOutput, randomBetween } from 'src/gameLogic/custom/functions/htmlHelper.functions';
 import { MasterService } from "src/app/service/master.service";
 import { factoryMap, factoryname } from "src/gameLogic/configurable/Factory/FactoryMap";
 import { gamesavenames } from "src/gameLogic/configurable/subservice/game-saver.type";
-import { enterRoomOption, nextOption } from 'src/gameLogic/custom/Class/Scene/CommonOptions';
 import { primitive } from '../types';
-import { escapeBattle } from 'src/gameLogic/custom/Class/Battle/Battle.functions';
-import { enterRoom } from 'src/gameLogic/custom/subservice/map-handler';
+import { FactoryFunction } from 'src/gameLogic/configurable/Factory/FactoryMap';
+import { ModuleFunctions } from "src/gameLogic/configurable/Factory/FactoryMap";
 
-/** @type {FactoryFunction&global_functions} */
+/** @type {FactoryFunction&ModuleFunctions} */
 export function Factory(masterService:MasterService,options:StoreableType)
 { return factoryMap[options.Factory](masterService,options) }
-Factory.randomCheck=randomCheck;
-Factory.randomBetween=randomBetween;
-Factory.pushBattleActionOutput=pushBattleActionOutput;
-Factory.escapeBattle=escapeBattle;
-Factory.enterRoom=enterRoom;
-enum options_names_enum {
-  nextOption="nextOption",
-  enterRoomOption="enterRoomOption",
-}
-Factory.options = {
-  nextOption,
-  enterRoomOption,
-};
+
 export type StoreableType = {Factory:factoryname,type: string,dependency_gamesave_object_key?:gamesavenames[],[key: string]:any};
 export interface storeable {
   type:primitive,
   toJson():StoreableType;
   fromJson(options:StoreableType):void;
-}
-export interface global_functions{
-  randomCheck:typeof randomCheck;
-  randomBetween:typeof randomBetween;
-  pushBattleActionOutput:typeof pushBattleActionOutput;
-  options:typeof Factory.options;
-  escapeBattle:typeof escapeBattle;
-  enterRoom:typeof enterRoom;
 }
