f201cd2e5e9dddb460001ee773bc11190fdb1b31
Laxharoth <zeydenlaxharoth@gmail.com>
2022-07-15 12:43:30 -0500

    Added character constanst for sceneParser

--- src/gameLogic/custom/customTypes/constants.ts
+++ src/gameLogic/custom/customTypes/constants.ts
@@ -1,2 +1,5 @@
 export const MAXOPTIONSNUMBERPERPAGE = 14;
 export const CHARACTERSELLITEMPRICEREDUCTION = 10;
+export const SCENE_PARSER_ELEMENT_SEPARATOR = '\\';
+export const SCENE_PARSER_STOPCHAR = Array.from(".#[{");
+export const SCENE_PARSER_ELEMENT_STOPCHAR = Array.from("+>^");
