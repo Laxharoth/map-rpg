d184be3d3c0f4afb7fdc1e6350a8e6be45ec4106
Laxharoth <zeydenlaxharoth@gmail.com>
2022-07-15 15:03:52 -0500

    Update room25.ts
    fix lint

--- src/Load/Map/TestMap/map2/room25.ts
+++ src/Load/Map/TestMap/map2/room25.ts
@@ -1,26 +1,25 @@
 import { MasterService } from "src/app/service/master.service";
 import { Scene } from "src/gameLogic/custom/Class/Scene/Scene";
-import { fill_room, Room, roomFunction } from "src/gameLogic/custom/Class/maps/room";
+import { Room, roomFunction } from "src/gameLogic/custom/Class/maps/room";
 
-export function room25(roomname: string): roomFunction
-{
+export function room25(roomname: string): roomFunction{
   return {
     roomname,
-    create:function(masterService:MasterService): Room{
+    create(masterService:MasterService): Room{
       const returnToMap1={text:"Map1",action:()=>{masterService.mapHandler.loadRoom("room24")},disabled:false}
       const roomScene: Scene = {
-        sceneData: function () {
+        sceneData () {
           return `Sign with ${roomname} written`
         },
         options: (roomname === 'room25') ? [returnToMap1] : [],
         fixedOptions: [null, null, null, null, null]
       }
-      return fill_room({
-        onEnter: function(){masterService.sceneHandler.tailScene(roomScene,'map').nextScene()},
-        onExit: function(){},
-        beforeMoveTo: function(roomname: string){return true},
-        afterMoveTo: function(roomname: string){},
-      })
+      return {
+        onEnter(){masterService.sceneHandler.tailScene(roomScene,'map').nextScene()},
+        onExit(){return undefined},
+        beforeMoveTo(){return true},
+        afterMoveTo(){return undefined},
+      }
     }
   }
 }
