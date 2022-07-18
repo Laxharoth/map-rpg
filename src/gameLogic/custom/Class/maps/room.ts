65a0c888c301ecb8c36cf139c204a334c53543d5
Laxharoth <zeydenlaxharoth@gmail.com>
2022-07-18 11:40:44 -0500

    Simplify Room comments

--- src/gameLogic/custom/Class/maps/room.ts
+++ src/gameLogic/custom/Class/maps/room.ts
@@ -1,46 +1,24 @@
 import { MasterService } from "src/app/service/master.service";
 import { Scene } from "../Scene/Scene";
 
-/** A model of a specific room */
+/** A model of a specific room
+ * * Room functions order:
+ * * onEnter
+ * * beforeMoveTo
+ * * onExit
+ * * afterMoveTo
+ */
 export interface Room{
-  /**
-   * The action to be performed when the room is entered.
-   * * Room functions order:
-   *   * onEnter
-   *   * beforeMoveTo
-   *   * onExit
-   *   * afterMoveTo
-   */
-     onEnter     : () => void;
-     /**
-      * The action to be performed when the room is exited.
-      * * Room functions order:
-      *   * onEnter
-      *   * beforeMoveTo
-      *   * onExit
-      *   * afterMoveTo
-      */
-     onExit      : () => void;
-     /**
-      * Perform an action before moving to a room.
-      * * Room functions order:
-      *   * onEnter
-      *   * beforeMoveTo
-      *   * onExit
-      *   * afterMoveTo
-      */
-     beforeMoveTo ?: (roomName:string) => boolean;
-     /**
-      * Perform an action after moving to a room.
-      * * Room functions order:
-      *   * onEnter
-      *   * beforeMoveTo
-      *   * onExit
-      *   * afterMoveTo
-      */
-     afterMoveTo ?: (roomName:string) => void;
-     icon        ?: string;
-     updateable_scene?:{[key: string]:Scene};
+  /** The action to be performed when the room is entered. */
+  onEnter     : () => void;
+  /** The action to be performed when the room is exited. */
+  onExit      : () => void;
+  /** Perform an action before moving to a room. */
+  beforeMoveTo ?: (roomName:string) => boolean;
+  /** Perform an action after moving to a room. */
+  afterMoveTo ?: (roomName:string) => void;
+  icon        ?: string;
+  updateable_scene?:{[key: string]:Scene};
 }
 
 export interface RoomFunction{
